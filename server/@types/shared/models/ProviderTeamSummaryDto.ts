f0f939fffeef8b64b8e4c862acb7e73065ec94bf
lookupdaily <50752284+lookupdaily@users.noreply.github.com>
2025-09-11 12:33:36 +0000

    Updating community-payback-api models from OpenAPI specification

---
+++ server/@types/shared/models/ProviderTeamSummaryDto.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ProviderTeamSummaryDto = {
+    /**
+     * Community Payback (UPW) provider team id
+     */
+    id: number;
+    /**
+     * Community Payback (UPW) provider team name
+     */
+    name: string;
+};
+
