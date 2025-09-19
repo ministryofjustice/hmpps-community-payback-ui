11f5b42399b1c7776e443ebc8e734d30c88c1571
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-09-19 17:28:25 +0100

    Implement reference data client and service

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,14 +1,16 @@
 import { dataAccess } from '../data'
 import AuditService from './auditService'
 import ProviderService from './providerService'
+import ReferenceDataService from './referenceDataService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient, providerClient } = dataAccess()
+  const { applicationInfo, hmppsAuditClient, providerClient, referenceDataClient } = dataAccess()
 
   return {
     applicationInfo,
     auditService: new AuditService(hmppsAuditClient),
     providerService: new ProviderService(providerClient),
+    referenceDataService: new ReferenceDataService(referenceDataClient),
   }
 }
 
