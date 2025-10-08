59335cca86ab2cd7e53d3b6ae4b7df9d7255b72e
Dan Liburd <daniel.liburd@gmail.com>
2025-10-08 14:57:08 +0100

    Add attendanceOutcome route handler
    This will render the attendance outcome page with all outcome options from the
    API.
    
    As we now have a separate path for the project details page, we rename this
    route handler and create a new update handler which currently just redirects to
    the homepage.

--- server/controllers/appointmentsController.test.ts
+++ server/controllers/appointmentsController.test.ts
@@ -3,9 +3,11 @@ import type { NextFunction, Request, Response } from 'express'
 
 import AppointmentService from '../services/appointmentService'
 import AppointmentsController from './appointmentsController'
-import { AppointmentDto, OffenderFullDto } from '../@types/shared'
+import { AppointmentDto, ContactOutcomesDto, OffenderFullDto } from '../@types/shared'
 import Offender from '../models/offender'
 import DateTimeFormats from '../utils/dateTimeUtils'
+import ReferenceDataService from '../services/referenceDataService'
+import paths from '../paths'
 
 jest.mock('../models/offender')
 
@@ -21,16 +23,17 @@ describe('AppointmentsController', () => {
 
   let appointmentsController: AppointmentsController
   const appointmentService = createMock<AppointmentService>()
+  const referenceDataService = createMock<ReferenceDataService>()
 
   beforeEach(() => {
     jest.resetAllMocks()
-    appointmentsController = new AppointmentsController(appointmentService)
+    appointmentsController = new AppointmentsController(appointmentService, referenceDataService)
     offenderMock.mockImplementation(() => {
       return offender
     })
   })
 
-  describe('update', () => {
+  describe('projectDetails', () => {
     it('should render the check project details page', async () => {
       const offenderResponse: OffenderFullDto = {
         crn: 'string',
@@ -58,7 +61,7 @@ describe('AppointmentsController', () => {
       const dateAndTime = '1 January 2025, 09:00 - 17:00'
       jest.spyOn(DateTimeFormats, 'dateAndTimePeriod').mockReturnValue(dateAndTime)
 
-      const requestHandler = appointmentsController.update()
+      const requestHandler = appointmentsController.projectDetails()
       await requestHandler(request, response, next)
 
       const project = {
@@ -73,5 +76,65 @@ describe('AppointmentsController', () => {
         offender,
       })
     })
+  })
+
+  describe('attendanceOutcome', () => {
+    it('should render the attendance outcome page', async () => {
+      const offenderResponse: OffenderFullDto = {
+        crn: 'string',
+        objectType: 'Full',
+        forename: 'string',
+        surname: 'string',
+        middleNames: [],
+      }
+      const appointment: AppointmentDto = {
+        id: 1001,
+        projectName: 'Community Garden Maintenance',
+        projectTypeName: 'Environmental Improvement',
+        projectTypeCode: 'ENV',
+        offender: offenderResponse,
+        supervisingTeam: 'Team Lincoln',
+        projectCode: 'XCT12',
+        date: '2025-01-02',
+        startTime: '11:00',
+        endTime: '12:00',
+      }
+      const contactOutcomes: ContactOutcomesDto = {
+        contactOutcomes: [
+          { name: 'Attended', id: 'outcome-id-1', code: 'CO1' },
+          { name: 'Did not attend', id: 'outcome-id-2', code: 'CO2' },
+        ],
+      }
+
+      const response = createMock<Response>()
+      const req = createMock<Request>({ params: { appointmentId: appointment.id.toString() } })
+
+      appointmentService.getAppointment.mockResolvedValue(appointment)
+      referenceDataService.getContactOutcomes.mockResolvedValue(contactOutcomes)
+
+      const requestHandler = appointmentsController.attendanceOutcome()
+      await requestHandler(req, response, next)
+
+      const expectedItems = [
+        {
+          text: 'Attended',
+          value: 'outcome-id-1',
+        },
+        {
+          text: 'Did not attend',
+          value: 'outcome-id-2',
+        },
+      ]
+
+      expect(response.render).toHaveBeenCalledWith('appointments/update/attendanceOutcome', {
+        offender,
+        items: expectedItems,
+        updatePath: paths.appointments.update({ appointmentId: appointment.id.toString() }),
+      })
+    })
+  })
+
+  describe('update', () => {
+    it.todo('should update the appointment')
   })
 })
--- server/controllers/appointmentsController.ts
+++ server/controllers/appointmentsController.ts
@@ -2,11 +2,16 @@ import type { Request, RequestHandler, Response } from 'express'
 import AppointmentService from '../services/appointmentService'
 import Offender from '../models/offender'
 import DateTimeFormats from '../utils/dateTimeUtils'
+import ReferenceDataService from '../services/referenceDataService'
+import paths from '../paths'
 
 export default class AppointmentsController {
-  constructor(private readonly appointmentService: AppointmentService) {}
+  constructor(
+    private readonly appointmentService: AppointmentService,
+    private readonly referenceDataService: ReferenceDataService,
+  ) {}
 
-  update(): RequestHandler {
+  projectDetails(): RequestHandler {
     return async (_req: Request, res: Response) => {
       const { appointmentId } = _req.params
       const appointment = await this.appointmentService.getAppointment(appointmentId, res.locals.user.username)
@@ -23,6 +28,30 @@ export default class AppointmentsController {
         project,
         offender,
       })
+    }
+  }
+
+  attendanceOutcome(): RequestHandler {
+    return async (_req: Request, res: Response) => {
+      const { appointmentId } = _req.params
+
+      const appointment = await this.appointmentService.getAppointment(appointmentId, res.locals.user.username)
+      const outcomes = await this.referenceDataService.getContactOutcomes(res.locals.user.username)
+      const offender = new Offender(appointment.offender)
+
+      const outcomeItems = outcomes.contactOutcomes.map(outcome => ({ text: outcome.name, value: outcome.id }))
+
+      res.render('appointments/update/attendanceOutcome', {
+        offender,
+        items: outcomeItems,
+        updatePath: paths.appointments.update({ appointmentId }),
+      })
+    }
+  }
+
+  update(): RequestHandler {
+    return async (_req: Request, res: Response) => {
+      res.redirect('/')
     }
   }
 }
