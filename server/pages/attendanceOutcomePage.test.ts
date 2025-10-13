cc2acb1dc8f0d33c55eb6bde4808f6c717a1f662
Dan Liburd <daniel.liburd@gmail.com>
2025-10-13 18:06:08 +0100

    Move contact outcomes creation to page model
    In order to simplify the logic in controller handlers, move the creation of the
    contact outcomes select items to the page model.

--- server/pages/attendanceOutcomePage.test.ts
+++ server/pages/attendanceOutcomePage.test.ts
@@ -10,4 +10,26 @@ describe('AttendanceOutcomePage', () => {
       })
     })
   })
+
+  describe('items', () => {
+    it('returns items for contact outcomes', () => {
+      const page = new AttendanceOutcomePage({} as AttendanceOutcomeBody)
+
+      const contactOutcomes = [
+        { name: 'Attended', id: 'outcome-id-1', code: 'CO1' },
+        { name: 'Did not attend', id: 'outcome-id-2', code: 'CO2' },
+      ]
+
+      expect(page.items(contactOutcomes)).toEqual([
+        {
+          text: 'Attended',
+          value: 'outcome-id-1',
+        },
+        {
+          text: 'Did not attend',
+          value: 'outcome-id-2',
+        },
+      ])
+    })
+  })
 })
