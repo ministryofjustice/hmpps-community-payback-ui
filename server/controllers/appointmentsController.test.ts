7ff1129c257097a918c9b6dc4f0e86ec8ea64a64
Dan Liburd <daniel.liburd@gmail.com>
2025-10-14 14:42:09 +0100

    Remove attendance outcome logic from appointments controller
    Now that we have a separate controller for attendance outcome, we can remove
    the logic from appointments.

--- server/controllers/appointmentsController.test.ts
+++ server/controllers/appointmentsController.test.ts
@@ -3,16 +3,14 @@ import type { NextFunction, Request, Response } from 'express'
 
 import AppointmentService from '../services/appointmentService'
 import AppointmentsController from './appointmentsController'
-import { ContactOutcomesDto } from '../@types/shared'
 import Offender from '../models/offender'
 import DateTimeFormats from '../utils/dateTimeUtils'
-import ReferenceDataService from '../services/referenceDataService'
-import paths from '../paths'
 import SessionUtils from '../utils/sessionUtils'
 import appointmentFactory from '../testutils/factories/appointmentFactory'
 import ProviderService from '../services/providerService'
 import GovUkSelectInput from '../forms/GovUkSelectInput'
 import supervisorSummaryFactory from '../testutils/factories/supervisorSummaryFactory'
+import paths from '../paths'
 
 jest.mock('../models/offender')
 
@@ -29,12 +27,11 @@ describe('AppointmentsController', () => {
 
   let appointmentsController: AppointmentsController
   const appointmentService = createMock<AppointmentService>()
-  const referenceDataService = createMock<ReferenceDataService>()
   const providerDataService = createMock<ProviderService>()
 
   beforeEach(() => {
     jest.resetAllMocks()
-    appointmentsController = new AppointmentsController(appointmentService, referenceDataService, providerDataService)
+    appointmentsController = new AppointmentsController(appointmentService, providerDataService)
     offenderMock.mockImplementation(() => {
       return offender
     })
@@ -140,47 +137,5 @@ describe('AppointmentsController', () => {
         }),
       )
     })
-  })
-
-  describe('attendanceOutcome', () => {
-    it('should render the attendance outcome page', async () => {
-      const appointment = appointmentFactory.build()
-      const contactOutcomes: ContactOutcomesDto = {
-        contactOutcomes: [
-          { name: 'Attended', id: 'outcome-id-1', code: 'CO1', enforceable: true },
-          { name: 'Did not attend', id: 'outcome-id-2', code: 'CO2', enforceable: false },
-        ],
-      }
-
-      const response = createMock<Response>()
-      const req = createMock<Request>({ params: { appointmentId: appointment.id.toString() } })
-
-      appointmentService.getAppointment.mockResolvedValue(appointment)
-      referenceDataService.getContactOutcomes.mockResolvedValue(contactOutcomes)
-
-      const requestHandler = appointmentsController.attendanceOutcome()
-      await requestHandler(req, response, next)
-
-      const expectedItems = [
-        {
-          text: 'Attended',
-          value: 'outcome-id-1',
-        },
-        {
-          text: 'Did not attend',
-          value: 'outcome-id-2',
-        },
-      ]
-
-      expect(response.render).toHaveBeenCalledWith('appointments/update/attendanceOutcome', {
-        offender,
-        items: expectedItems,
-        updatePath: paths.appointments.update({ appointmentId: appointment.id.toString() }),
-      })
-    })
-  })
-
-  describe('update', () => {
-    it.todo('should update the appointment')
   })
 })
