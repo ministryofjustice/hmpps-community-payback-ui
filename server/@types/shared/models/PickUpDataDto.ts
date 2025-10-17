7f42e23d24ff74b190d63f77943a9f1c383de647
Toby Batch <toby.batch@digital.justice.gov.uk>
2025-10-17 11:46:50 +0100

    CPB-129(fix): updates DTOs to match added api changes

---
+++ server/@types/shared/models/PickUpDataDto.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { LocationDto } from './LocationDto';
+export type PickUpDataDto = {
+    location?: LocationDto;
+    time?: string;
+};
+
