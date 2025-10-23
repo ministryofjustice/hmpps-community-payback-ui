9e8bdb429e25924d16b86a4c0c67bd0b393791be
Dan Liburd <daniel.liburd@gmail.com>
2025-10-23 16:34:42 +0100

    Require passing in a session and appointment to stubs
    The calling test now requires an object to be supplied when stubbing these
    endpoints to make it more flexible and explicit.

--- integration_tests/mockApis/sessions.ts
+++ integration_tests/mockApis/sessions.ts
@@ -37,71 +37,19 @@ export default {
       },
     })
   },
-  stubFindSession: ({
-    projectCode = 'prj',
-    date = '2025-09-07',
-    responseHasLimitedOffenders = false,
-  }: {
-    projectCode: string
-    date: string
-    responseHasLimitedOffenders: boolean
-  }): SuperAgentRequest => {
-    const pattern = paths.projects.sessionAppointments({ projectCode, date })
+  stubFindSession: ({ session }: { session: SessionDto }): SuperAgentRequest => {
+    const pattern = paths.projects.sessionAppointments({ projectCode: session.projectCode, date: session.date })
     return stubFor({
       request: {
         method: 'GET',
-        urlPathPattern: pattern,
+        urlPath: pattern,
       },
       response: {
         status: 200,
         headers: { 'Content-Type': 'application/json;charset=UTF-8' },
-        jsonBody: {
-          ...mockAppointments(responseHasLimitedOffenders),
-        },
+        jsonBody: session,
       },
     })
   },
 }
 
-export const mockAppointments = (hasLimitedOffenders: boolean = false): SessionDto => ({
-  projectName: 'Park cleaning',
-  projectCode: 'XCT12',
-  projectLocation: 'Hammersmith',
-  date: '2025-01-02',
-  startTime: '11:00',
-  endTime: '12:00',
-  appointmentSummaries: [
-    {
-      id: 1001,
-      requirementMinutes: 600,
-      completedMinutes: 500,
-      offender: hasLimitedOffenders
-        ? {
-            crn: 'CRN123',
-            objectType: 'Limited',
-          }
-        : {
-            forename: 'John',
-            surname: 'Smith',
-            crn: 'CRN123',
-            objectType: 'Full',
-          },
-    },
-    {
-      id: 1002,
-      requirementMinutes: 900,
-      completedMinutes: 600,
-      offender: hasLimitedOffenders
-        ? {
-            crn: 'CRN124',
-            objectType: 'Limited',
-          }
-        : {
-            forename: 'Roberta',
-            surname: 'John',
-            crn: 'CRN124',
-            objectType: 'Full',
-          },
-    },
-  ],
-})
