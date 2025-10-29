d2e0a531627daba2e4ff5ddcf189cf2ecabbe97a
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-10-29 10:31:37 +0000

    Add e2e test for enforceable attendance outcome
    If an enforceable attendance outcome is selected, the user will see a slightly
    different form journey.
    
    This should be updated with continuing to confirm and submit once that has been
    handled.

--- e2e-tests/pages/appointments/attendanceOutcomePage.ts
+++ e2e-tests/pages/appointments/attendanceOutcomePage.ts
@@ -4,9 +4,16 @@ import AppointmentFormPage from './appointmentFormPage'
 export default class AttendanceOutcomePage extends AppointmentFormPage {
   radioOptionsLocator: Locator
 
+  outcomeWithEnforcementLocator: Locator
+
   constructor(page: Page) {
     super(page, 'Log attendance')
     this.radioOptionsLocator = page.getByRole('radio')
+    this.outcomeWithEnforcementLocator = page.getByLabel('Unacceptable Absence')
+  }
+
+  async chooseEnforcementOutcome() {
+    await this.outcomeWithEnforcementLocator.check()
   }
 
   async chooseOutcome() {
---
+++ e2e-tests/tests/02_update_appointment_enforcement.spec.ts
@@ -0,0 +1,32 @@
+import test from '../test'
+import signIn from '../steps/signIn'
+import searchForASession from '../steps/searchForASession'
+import selectASession from '../steps/selectASession'
+import clickUpdateAnAppointment from '../steps/clickUpdateAnAppointment'
+import completeCheckProjectDetails from '../steps/completeCheckProjectDetails'
+import completeHours from '../steps/completeHours'
+import completeCompliance from '../steps/completeCompliance'
+import completeAttendanceOutcomeWithEnforcement from '../steps/completeAttendanceOutcomeWithEnforcement'
+import EnforcementPage from '../pages/appointments/enforcementPage'
+
+test('Update a session appoinment with an enforceable outcome', async ({ page, deliusUser }) => {
+  const homePage = await signIn(page, deliusUser)
+  const trackProgressPage = await searchForASession(page, homePage)
+
+  await trackProgressPage.expect.toSeeResults()
+
+  const sessionPage = await selectASession(page, trackProgressPage)
+
+  await sessionPage.expect.toSeeAppointments()
+
+  const checkProjectDetailsPage = await clickUpdateAnAppointment(page, sessionPage)
+  const attendanceOutcomePage = await completeCheckProjectDetails(page, checkProjectDetailsPage)
+
+  const logHoursPage = await completeAttendanceOutcomeWithEnforcement(page, attendanceOutcomePage)
+  const logCompliancePage = await completeHours(page, logHoursPage)
+
+  await completeCompliance(page, logCompliancePage)
+
+  const enforcementPage = new EnforcementPage(page)
+  await enforcementPage.expect.toBeOnThePage()
+})
