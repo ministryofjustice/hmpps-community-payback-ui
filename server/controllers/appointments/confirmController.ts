9a7b286e9d86bfacedbc427756fddcffc11267d3
Dan Liburd <daniel.liburd@gmail.com>
2025-10-30 14:12:52 +0000

    Handle saving of appointment

--- server/controllers/appointments/confirmController.ts
+++ server/controllers/appointments/confirmController.ts
@@ -2,6 +2,8 @@ import type { Request, RequestHandler, Response } from 'express'
 import AppointmentService from '../../services/appointmentService'
 import AppointmentFormService from '../../services/appointmentFormService'
 import ConfirmPage from '../../pages/appointments/confirmPage'
+import { UpdateAppointmentOutcomeDto } from '../../@types/shared'
+import SessionUtils from '../../utils/sessionUtils'
 
 export default class ConfirmController {
   constructor(
@@ -23,7 +25,29 @@ export default class ConfirmController {
 
   submit(): RequestHandler {
     return async (_req: Request, res: Response) => {
-      return res.redirect('/')
+      const { appointmentId } = _req.params
+      const appointment = await this.appointmentService.getAppointment(appointmentId, res.locals.user.username)
+
+      const page = new ConfirmPage(_req.query)
+      const form = await this.appointmentFormService.getForm(page.formId, res.locals.user.name)
+
+      const payload: UpdateAppointmentOutcomeDto = {
+        deliusId: appointment.id,
+        deliusVersionToUpdate: appointment.version,
+        alertActive: appointment.alertActive,
+        sensitive: appointment.sensitive,
+        startTime: form.data.startTime,
+        endTime: form.data.endTime,
+        contactOutcomeId: form.data.contactOutcome.id,
+        attendanceData: form.data.attendanceData,
+        supervisorOfficerCode: form.data.supervisorOfficerCode,
+        notes: form.data.notes,
+        formKeyToDelete: form.key,
+      }
+
+      await this.appointmentService.saveAppointment(payload, res.locals.user.name)
+
+      return res.redirect(SessionUtils.getSessionPath(appointment))
     }
   }
 }
