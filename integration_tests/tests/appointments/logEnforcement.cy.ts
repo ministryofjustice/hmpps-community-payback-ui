0392c5112a54bdf088c2dde2a5fd0ee877152b53
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-11-03 19:10:55 +0000

    Add missing integration tests
    We were missing a test for continuing and navigating back from the enforcement
    page.

--- integration_tests/tests/appointments/logEnforcement.cy.ts
+++ integration_tests/tests/appointments/logEnforcement.cy.ts
@@ -9,23 +9,25 @@
 //    When I submit the form
 //    Then I see the same page with errors
 
+//  Scenario: submitting enforcement page
+//    Given I am on an the enforcement actions form page
+//    When I submit the form
+//    Then I see the confirm page
+
+//  Scenario: navigating back
+//    Scenario: appointment was attended
+//    Given I am on an the enforcement actions form page for an appointment which was attended
+//    When I submit the form
+//    Then I see the compliance form page
+
 import appointmentFactory from '../../../server/testutils/factories/appointmentFactory'
 import EnforcementPage from '../../pages/appointments/enforcementPage'
-import { enforcementActionFactory } from '../../../server/testutils/factories/enforcementActionFactory'
-
-context('Log compliance', () => {
-  beforeEach(() => {
-    cy.task('reset')
-    cy.task('stubSignIn')
-    cy.signIn()
-
-    const appointment = appointmentFactory.build()
-    cy.wrap(appointment).as('appointment')
-
-    const enforcementActions = { enforcementActions: enforcementActionFactory.buildList(2) }
-    cy.wrap(enforcementActions).as('enforcementActions')
-  })
-})
+import { enforcementActionsFactory } from '../../../server/testutils/factories/enforcementActionFactory'
+import appointmentOutcomeFormFactory from '../../../server/testutils/factories/appointmentOutcomeFormFactory'
+import Page from '../../pages/page'
+import ConfirmDetailsPage from '../../pages/appointments/confirmDetailsPage'
+import { contactOutcomeFactory } from '../../../server/testutils/factories/contactOutcomeFactory'
+import LogCompliancePage from '../../pages/appointments/logCompliancePage'
 
 context('Log enforcement', () => {
   beforeEach(() => {
@@ -33,7 +35,7 @@ context('Log enforcement', () => {
     cy.task('stubSignIn')
     cy.signIn()
 
-    const enforcementActions = { enforcementActions: enforcementActionFactory.buildList(2) }
+    const enforcementActions = enforcementActionsFactory.build()
     cy.wrap(enforcementActions).as('enforcementActions')
   })
 
@@ -53,5 +55,49 @@ context('Log enforcement', () => {
 
     // Then I see the same page with errors
     page.shouldShowErrorSummary('respondBy-day', 'The date to respond by must include a day, month and year')
+  })
+
+  // Scenario: submitting enforcement page
+  describe('submit', function action() {
+    it('continues to confirm page', function test() {
+      const form = appointmentOutcomeFormFactory.build()
+      // Given I am on an the enforcement actions form page
+      const appointment = appointmentFactory.build()
+
+      cy.task('stubFindAppointment', { appointment })
+      cy.task('stubGetEnforcementActions', { enforcementActions: this.enforcementActions })
+      cy.task('stubGetForm', form)
+      cy.task('stubSaveForm')
+      const page = EnforcementPage.visit(appointment)
+
+      // When I submit the form
+      page.clickSubmit()
+
+      // Then I see the confirm page
+      Page.verifyOnPage(ConfirmDetailsPage, appointment)
+    })
+  })
+
+  // Scenario: navigating back
+  describe('back', function action() {
+    // Scenario: appointment was attended
+    it('attended => navigates to compliance', function test() {
+      const form = appointmentOutcomeFormFactory.build({
+        contactOutcome: contactOutcomeFactory.build({ attended: true }),
+      })
+      // Given I am on an the enforcement actions form page for an appointment which was attended
+      const appointment = appointmentFactory.build()
+
+      cy.task('stubFindAppointment', { appointment })
+      cy.task('stubGetEnforcementActions', { enforcementActions: this.enforcementActions })
+      cy.task('stubGetForm', form)
+      const page = EnforcementPage.visit(appointment)
+
+      // When I submit the form
+      page.clickBack()
+
+      // Then I see the the compliance form page
+      Page.verifyOnPage(LogCompliancePage, appointment)
+    })
   })
 })
