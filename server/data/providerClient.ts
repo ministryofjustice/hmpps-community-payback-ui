9572462929ab3a648ada8074c56d992aa6bf7c73
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-11-05 17:43:23 +0000

    Add service to get list of providers

--- server/data/providerClient.ts
+++ server/data/providerClient.ts
@@ -5,11 +5,18 @@ import config from '../config'
 import logger from '../../logger'
 import paths from '../paths/api'
 import { GetForTeamRequest } from '../@types/user-defined'
-import { SupervisorSummariesDto } from '../@types/shared'
+import { ProviderSummariesDto, SupervisorSummariesDto } from '../@types/shared'
+import { createQueryString } from '../utils/utils'
 
 export default class ProviderClient extends RestClient {
   constructor(authenticationClient: AuthenticationClient) {
     super('providerClient', config.apis.communityPaybackApi, logger, authenticationClient)
+  }
+
+  async getProviders(username: string): Promise<ProviderSummariesDto> {
+    const path = paths.providers.providers.pattern
+    const query = createQueryString({ username })
+    return (await this.get({ path, query }, asSystem(username))) as ProviderSummariesDto
   }
 
   async getTeams(providerCode: string, username: string): Promise<ProviderTeamSummariesDto> {
--- server/services/providerService.ts
+++ server/services/providerService.ts
@@ -1,9 +1,14 @@
-import { ProviderTeamSummariesDto, SupervisorSummaryDto } from '../@types/shared'
+import { ProviderSummaryDto, ProviderTeamSummariesDto, SupervisorSummaryDto } from '../@types/shared'
 import { GetForTeamRequest } from '../@types/user-defined'
 import ProviderClient from '../data/providerClient'
 
 export default class ProviderService {
   constructor(private readonly providerClient: ProviderClient) {}
+
+  async getProviders(username: string): Promise<ProviderSummaryDto[]> {
+    const response = await this.providerClient.getProviders(username)
+    return response.providers
+  }
 
   async getTeams(providerCode: string, username: string): Promise<ProviderTeamSummariesDto> {
     const teams = await this.providerClient.getTeams(providerCode, username)
