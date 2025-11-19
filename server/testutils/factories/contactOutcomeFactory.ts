33c85d3f891eb214838f9c69ad735d14f799d984
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-11-19 08:22:14 +0000

    Change faker type to alpha string rather than sample
    I believe this was causing flakey test results as sample strings have special characters in them.

--- server/testutils/factories/contactOutcomeFactory.ts
+++ server/testutils/factories/contactOutcomeFactory.ts
@@ -5,7 +5,7 @@ import { ContactOutcomeDto, ContactOutcomesDto } from '../../@types/shared'
 export const contactOutcomeFactory = Factory.define<ContactOutcomeDto>(() => ({
   id: faker.string.uuid(),
   name: faker.company.name(),
-  code: faker.string.sample(10),
+  code: faker.string.alpha(10),
   enforceable: faker.datatype.boolean(),
   attended: faker.datatype.boolean(),
   availableToSupervisors: faker.datatype.boolean(),
