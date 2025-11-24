0ba5ab8b9c5528b785b280b280a51c5da8c97718
Ben Shimmin <bas@bas.me.uk>
2025-11-24 13:47:46 +0000

    Remove "not applicable" from hi-vis options

--- server/pages/appointments/logCompliancePage.test.ts
+++ server/pages/appointments/logCompliancePage.test.ts
@@ -249,7 +249,7 @@ describe('LogCompliancePage', () => {
       const form = {}
 
       const query: LogComplianceQuery = {
-        hiVis: 'yes',
+        hiVis: 'no',
         workedIntensively: 'no',
         workQuality: 'EXCELLENT',
         behaviour: 'GOOD',
@@ -262,7 +262,7 @@ describe('LogCompliancePage', () => {
 
       const expected: AppointmentOutcomeForm = {
         attendanceData: {
-          hiVisWorn: true,
+          hiVisWorn: false,
           workedIntensively: false,
           workQuality: 'EXCELLENT',
           behaviour: 'GOOD',
@@ -277,7 +277,7 @@ describe('LogCompliancePage', () => {
     it('updates and returns data from query given object with existing data', () => {
       const form = { startTime: '10:00', attendanceData: { penaltyTime: '01:00' } } as AppointmentOutcomeForm
       const query: LogComplianceQuery = {
-        hiVis: 'yes',
+        hiVis: 'no',
         workedIntensively: 'no',
         workQuality: 'EXCELLENT',
         behaviour: 'GOOD',
@@ -292,7 +292,7 @@ describe('LogCompliancePage', () => {
         startTime: '10:00',
         attendanceData: {
           penaltyTime: '01:00',
-          hiVisWorn: true,
+          hiVisWorn: false,
           workedIntensively: false,
           workQuality: 'EXCELLENT',
           behaviour: 'GOOD',
