e16e80f393eb949558200f19ce596f9175ea7013
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-01-22 10:49:20 +0000

    Introduce radio group page object model
    I want to add a few actions and assertions related to a single radio group, so
    adding a component model to keep this readable and repeatable.

---
+++ integration_tests/pages/components/radioGroupComponent.ts
@@ -0,0 +1,23 @@
+export default class RadioGroupComponent {
+  constructor(private readonly name: string) {}
+
+  getAll(): Cypress.Chainable<JQuery<HTMLElement>> {
+    return cy.get(`input[name="${this.name}"]`)
+  }
+
+  getOptionWithValue(value: string): Cypress.Chainable<JQuery<HTMLElement>> {
+    return cy.get(`input[name="${this.name}"][value="${value}"]`)
+  }
+
+  checkOptionWithValue(value: string) {
+    this.getOptionWithValue(value).check()
+  }
+
+  shouldHaveSelectedValue(value: string) {
+    this.getOptionWithValue(value).should('be.checked')
+  }
+
+  shouldNotHaveASelectedValue() {
+    this.getAll().should('not.be.checked')
+  }
+}
