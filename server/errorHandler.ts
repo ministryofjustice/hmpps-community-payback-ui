b3bad894082e0b7c463373351a365ab656c077fc
Patrick Fleming <patrick.fleming@dxw.com>
2026-01-23 10:12:01 +0000

    Update error handling to render 404 and 500 error templates
    The GDS templates will now be rendered for 404 and 500 errors in production only. I’ve left the rest of the error handler as is to catch anything else that’s unhandled, and to maintain the stack trace for development and maintenance in all other environments.

--- server/errorHandler.ts
+++ server/errorHandler.ts
@@ -1,23 +1,39 @@
 import type { Request, Response, NextFunction } from 'express'
-import type { HTTPError } from 'superagent'
+import { HTTPError } from 'superagent'
+import { SanitisedError } from '@ministryofjustice/hmpps-rest-client'
 import logger from '../logger'
+import { getErrorStatus } from './utils/errorUtils'
 
 export default function createErrorHandler(production: boolean) {
-  return (error: HTTPError, req: Request, res: Response, next: NextFunction): void => {
+  // next() is not used here but is required for the middleware to be discovered
+  return (error: HTTPError | SanitisedError, req: Request, res: Response, next: NextFunction): void => {
     logger.error(`Error handling request for '${req.originalUrl}', user '${res.locals.user?.username}'`, error)
 
-    if (error.status === 401 || error.status === 403) {
+    const errorStatus = getErrorStatus(error)
+
+    if (errorStatus === 401 || errorStatus === 403) {
       logger.info('Logging user out')
       return res.redirect('/sign-out')
+    }
+
+    if (production) {
+      if (errorStatus === 404) {
+        res.status(404)
+        return res.render('pages/404')
+      }
+      if (errorStatus === 500) {
+        res.status(500)
+        return res.render('pages/500')
+      }
     }
 
     res.locals.message = production
       ? 'Something went wrong. The error has been logged. Please try again'
       : error.message
-    res.locals.status = error.status
+    res.locals.status = errorStatus
     res.locals.stack = production ? null : error.stack
 
-    res.status(error.status || 500)
+    res.status(errorStatus || 500)
 
     return res.render('pages/error')
   }
