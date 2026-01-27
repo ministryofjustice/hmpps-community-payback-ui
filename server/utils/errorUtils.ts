e080e21e685d8723abe503cc1ae5be6c72e02a43
Patrick Fleming <patrick.fleming@dxw.com>
2026-01-27 12:02:37 +0000

    Add error status utils
    These utils are largely copied from HMPPS activities management - https://github.com/ministryofjustice/hmpps-activities-management/blob/main/server/utils/helpers/errorHelpers.ts
    
    We need them because the HTTPErrors generated by Superagent and the SanitisedErrors of the HMPPS ecosystem have different types, with the former storing the error status under the key `status` and the latter under `responseStatus`. We could implement functions for sanitising all errors within our codebase so that they conform to the same type, but I think that for now that is overkill and we should wait until we’re sure we have that need.

--- server/utils/errorUtils.ts
+++ server/utils/errorUtils.ts
@@ -1,3 +1,6 @@
+import { SanitisedError } from '@ministryofjustice/hmpps-rest-client'
+import { HTTPError } from 'superagent'
+
 type ValidationError = {
   [key: string]: { text: string }
 }
@@ -10,4 +13,25 @@ export const generateErrorSummary = <T extends ValidationError>(validationErrors
       [`data-cy-error-${k}`]: validationErrors[k].text,
     },
   }))
+}
+
+/*
+  The errors we get sometimes have the status in .status but also can the status on .responseStatus.
+  As a result we need to handle both in the same way as an "any" type to prevent type errors where 
+  there's no overlap.
+*/
+export const getErrorStatus = (error: HTTPError | SanitisedError): number | undefined => {
+  if ('status' in error) {
+    return error.status
+  }
+
+  if ('responseStatus' in error) {
+    return error.responseStatus
+  }
+
+  return undefined
+}
+
+export const errorHasStatus = (error: HTTPError | SanitisedError, status: number): boolean => {
+  return getErrorStatus(error) === status
 }
