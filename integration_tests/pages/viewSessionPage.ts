a7031b2f622a94d14a95d16770bd98875d6dc86b
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-02-06 17:28:21 +0000

    Add datatable component for integration tests
    Refactor existing page assertion for a table to use new component.
    Includes a common method for checking data in table rows.

--- integration_tests/pages/viewSessionPage.ts
+++ integration_tests/pages/viewSessionPage.ts
@@ -1,18 +1,22 @@
-import { AppointmentSummaryDto, OffenderFullDto, SessionDto } from '../../server/@types/shared'
+import { OffenderFullDto, SessionDto } from '../../server/@types/shared'
 import paths from '../../server/paths'
 
 import Page from './page'
 import SummaryListComponent from './components/summaryListComponent'
 import DateTimeFormats from '../../server/utils/dateTimeUtils'
+import DataTableComponent from './components/datatableComponent'
 
 export default class ViewSessionPage extends Page {
   private readonly sessionDetails: SummaryListComponent
 
   private readonly session: SessionDto
 
+  sessionList: DataTableComponent
+
   constructor(session: SessionDto) {
     super(session.projectName)
     this.sessionDetails = new SummaryListComponent()
+    this.sessionList = new DataTableComponent()
     this.session = session
   }
 
@@ -39,34 +43,44 @@ export default class ViewSessionPage extends Page {
 
   shouldShowAppointmentsList() {
     const { appointmentSummaries } = this.session
+    const expectedRowValues = appointmentSummaries.map(appointmentSummary => {
+      const offender = appointmentSummary.offender as OffenderFullDto
 
-    cy.get('tr')
-      .eq(1)
-      .within(() => {
-        this.shouldShowAppointmentDetails(appointmentSummaries[0])
-      })
+      return [
+        `${offender.forename} ${offender.surname}`,
+        offender.crn,
+        DateTimeFormats.minutesToHoursAndMinutes(appointmentSummary.requirementMinutes),
+        DateTimeFormats.minutesToHoursAndMinutes(appointmentSummary.completedMinutes),
+        DateTimeFormats.minutesToHoursAndMinutes(
+          appointmentSummary.requirementMinutes -
+            appointmentSummary.completedMinutes +
+            appointmentSummary.adjustmentMinutes,
+        ),
+        appointmentSummary.contactOutcome.name,
+      ]
+    })
 
-    cy.get('tr')
-      .eq(2)
-      .within(() => {
-        this.shouldShowAppointmentDetails(appointmentSummaries[1])
-      })
+    this.sessionList.shouldHaveRowsWithContent(expectedRowValues)
   }
 
   shouldShowOffendersWithNoNames() {
     const { appointmentSummaries } = this.session
+    const expectedRowValues = appointmentSummaries.map(appointmentSummary => {
+      return [
+        '',
+        appointmentSummary.offender.crn,
+        DateTimeFormats.minutesToHoursAndMinutes(appointmentSummary.requirementMinutes),
+        DateTimeFormats.minutesToHoursAndMinutes(appointmentSummary.completedMinutes),
+        DateTimeFormats.minutesToHoursAndMinutes(
+          appointmentSummary.requirementMinutes -
+            appointmentSummary.completedMinutes +
+            appointmentSummary.adjustmentMinutes,
+        ),
+        appointmentSummary.contactOutcome.name,
+      ]
+    })
 
-    cy.get('tr')
-      .eq(1)
-      .within(() => {
-        this.shouldShowLimitedAppointmentDetails(appointmentSummaries[0])
-      })
-
-    cy.get('tr')
-      .eq(2)
-      .within(() => {
-        this.shouldShowLimitedAppointmentDetails(appointmentSummaries[1])
-      })
+    this.sessionList.shouldHaveRowsWithContent(expectedRowValues)
   }
 
   shouldNotHaveUpdateLinksForOffenders() {
@@ -77,51 +91,5 @@ export default class ViewSessionPage extends Page {
     cy.get('[data-testid="session-show-error-summary"]').within(() => {
       cy.get('a').contains(message)
     })
-  }
-
-  private shouldShowAppointmentDetails(appointmentSummary: AppointmentSummaryDto) {
-    const offender = appointmentSummary.offender as OffenderFullDto
-
-    cy.get('td').eq(0).should('have.text', `${offender.forename} ${offender.surname}`)
-    cy.get('td').eq(1).should('have.text', offender.crn)
-    cy.get('td')
-      .eq(2)
-      .should('have.text', DateTimeFormats.minutesToHoursAndMinutes(appointmentSummary.requirementMinutes))
-    cy.get('td')
-      .eq(3)
-      .should('have.text', DateTimeFormats.minutesToHoursAndMinutes(appointmentSummary.completedMinutes))
-    cy.get('td')
-      .eq(4)
-      .should(
-        'have.text',
-        DateTimeFormats.minutesToHoursAndMinutes(
-          appointmentSummary.requirementMinutes -
-            appointmentSummary.completedMinutes +
-            appointmentSummary.adjustmentMinutes,
-        ),
-      )
-    cy.get('td').eq(5).should('have.text', appointmentSummary.contactOutcome.name)
-  }
-
-  private shouldShowLimitedAppointmentDetails(appointmentSummary: AppointmentSummaryDto) {
-    cy.get('td').eq(0).should('have.text', '')
-    cy.get('td').eq(1).should('have.text', appointmentSummary.offender.crn)
-    cy.get('td')
-      .eq(2)
-      .should('have.text', DateTimeFormats.minutesToHoursAndMinutes(appointmentSummary.requirementMinutes))
-    cy.get('td')
-      .eq(3)
-      .should('have.text', DateTimeFormats.minutesToHoursAndMinutes(appointmentSummary.completedMinutes))
-    cy.get('td')
-      .eq(4)
-      .should(
-        'have.text',
-        DateTimeFormats.minutesToHoursAndMinutes(
-          appointmentSummary.requirementMinutes -
-            appointmentSummary.completedMinutes +
-            appointmentSummary.adjustmentMinutes,
-        ),
-      )
-    cy.get('td').eq(5).should('have.text', appointmentSummary.contactOutcome.name)
   }
 }
