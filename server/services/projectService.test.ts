5c1e7de45c9712111081cb026b1d2c3f79cf05f8
Dan Liburd <daniel.liburd@gmail.com>
2026-02-09 11:56:03 +0000

    Introduce ProjectService.getIndividualPlacementProjects method

--- server/services/projectService.test.ts
+++ server/services/projectService.test.ts
@@ -1,6 +1,7 @@
 import ProjectClient from '../data/projectClient'
 import ProjectService from './projectService'
 import projectFactory from '../testutils/factories/projectFactory'
+import pagedModelProjectSummaryFactory from '../testutils/factories/pagedModelProjectSummaryFactory'
 
 jest.mock('../data/projectClient')
 
@@ -12,16 +13,41 @@ describe('ProjectService', () => {
     projectService = new ProjectService(projectClient)
   })
 
-  it('should call find on the client and return its result', async () => {
-    const project = projectFactory.build()
+  describe('getProject', () => {
+    it('should call find on the client and return its result', async () => {
+      const project = projectFactory.build()
 
-    projectClient.find.mockResolvedValue(project)
-    const result = await projectService.getProject({
-      username: 'some-username',
-      projectCode: '1',
+      projectClient.find.mockResolvedValue(project)
+      const result = await projectService.getProject({
+        username: 'some-username',
+        projectCode: '1',
+      })
+
+      expect(projectClient.find).toHaveBeenCalledTimes(1)
+      expect(result).toEqual(project)
     })
+  })
 
-    expect(projectClient.find).toHaveBeenCalledTimes(1)
-    expect(result).toEqual(project)
+  describe('getIndividualPlacementProjects', () => {
+    it('should call `getProjects` on the client with "INDIVIDUAL" project type group and return its result', async () => {
+      const projects = pagedModelProjectSummaryFactory.build()
+
+      projectClient.getProjects.mockResolvedValue(projects)
+
+      const result = await projectService.getIndividualPlacementProjects({
+        username: 'some-username',
+        providerCode: '1',
+        teamCode: '123',
+      })
+
+      expect(projectClient.getProjects).toHaveBeenCalledTimes(1)
+      expect(projectClient.getProjects).toHaveBeenCalledWith({
+        username: 'some-username',
+        providerCode: '1',
+        teamCode: '123',
+        projectTypeGroup: 'INDIVIDUAL',
+      })
+      expect(result).toEqual(projects)
+    })
   })
 })
