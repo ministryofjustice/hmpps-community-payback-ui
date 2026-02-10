7174816e322d1fe5d6ff7ca70248e0902ce66a5d
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-02-10 15:36:26 +0000

    Add update link to project appointment list

--- integration_tests/tests/projects/viewAnIndvidualPlacement.cy.ts
+++ integration_tests/tests/projects/viewAnIndvidualPlacement.cy.ts
@@ -4,9 +4,10 @@
 //    I want to view details about a project
 //    And view any missing outcomes I need to capture
 //
-//  Scenario: viewing an individual placement
+//  Scenario: Viewing and updating an individual placement's appointments
 //    Given I am on the project page
-//    Then I should see the project details
+//    When I click on 'Update' for an appointment
+//    Then I should see the start of the appointment update journey
 //
 //  Scenario: navigating back from an individual placement
 //    Given I am on the project page
@@ -19,6 +20,10 @@ import pagedModelAppointmentSummaryFactory from '../../../server/testutils/facto
 import Page from '../../pages/page'
 import FindIndividualPlacementPage from '../../pages/projects/findIndividualPlacementPage'
 import { baseProjectAppointmentRequest } from '../../mockApis/projects'
+import CheckProjectDetailsPage from '../../pages/appointments/checkProjectDetailsPage'
+import supervisorSummaryFactory from '../../../server/testutils/factories/supervisorSummaryFactory'
+import appointmentFactory from '../../../server/testutils/factories/appointmentFactory'
+import Utils from '../../utils'
 
 context('Project page', () => {
   const project = projectFactory.build()
@@ -33,14 +38,29 @@ context('Project page', () => {
     cy.task('stubGetAppointments', { request, pagedAppointments })
   })
 
-  //  Scenario: viewing an individual placement
+  //  Scenario: Viewing and updating an individual placement's appointments
   it('shows project details', () => {
     //  Given I am on the project page
     const page = ProjectPage.visit(project)
-
-    //  Then I should see the project details
     page.shouldShowProjectDetails()
     page.shouldShowAppointmentsWithMissingOutcomes(pagedAppointments.content)
+
+    // When I click on 'Update' for an appointment
+    const [selected] = [...pagedAppointments.content].sort(Utils.sortByDate)
+    const appointment = appointmentFactory.build({ projectCode: project.projectCode, id: selected.id })
+    const supervisors = supervisorSummaryFactory.buildList(2)
+    cy.task('stubFindAppointment', { appointment })
+    cy.task('stubGetSupervisors', {
+      teamCode: appointment.supervisingTeamCode,
+      providerCode: appointment.providerCode,
+      supervisors,
+    })
+    cy.task('stubSaveForm')
+
+    page.clickUpdateAnAppointment()
+
+    // Then I should see the start of the appointment update journey
+    Page.verifyOnPage(CheckProjectDetailsPage, appointment)
   })
 
   //  Scenario: navigating back from an individual placement
