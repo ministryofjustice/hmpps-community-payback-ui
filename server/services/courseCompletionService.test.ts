c4b3ce027ee753417cd38e044eb6400e477b063e
Patrick Fleming <patrick.fleming@dxw.com>
2026-02-10 10:49:56 +0000

    Add searchCourseCompletions method to course completions service
    This method calls the client method and returns the data.

--- server/services/courseCompletionService.test.ts
+++ server/services/courseCompletionService.test.ts
@@ -1,5 +1,6 @@
 import CourseCompletionClient from '../data/courseCompletionClient'
 import courseCompletionFactory from '../testutils/factories/courseCompletionFactory'
+import pagedModelCourseCompletionEventFactory from '../testutils/factories/pagedModelCourseCompletionEventFactory'
 import CourseCompletionService from './courseCompletionService'
 
 jest.mock('../data/courseCompletionClient')
@@ -24,5 +25,21 @@ describe('CourseCompletionService', () => {
 
     expect(courseCompletionClient.find).toHaveBeenCalledTimes(1)
     expect(result).toEqual(courseCompletion)
+  })
+
+  it('should call getCourseCompletions on the api client and return its result', async () => {
+    const courseCompletionsPagedResponse = pagedModelCourseCompletionEventFactory.build()
+
+    courseCompletionClient.getCourseCompletions.mockResolvedValue(courseCompletionsPagedResponse)
+
+    const result = await courseCompletionService.searchCourseCompletions({
+      username: 'some-username',
+      providerCode: 'A1234',
+      dateFrom: '2025-09-01',
+      dateTo: '2025-09-02',
+    })
+
+    expect(courseCompletionClient.getCourseCompletions).toHaveBeenCalledTimes(1)
+    expect(result).toEqual(courseCompletionsPagedResponse)
   })
 })
--- server/services/courseCompletionService.ts
+++ server/services/courseCompletionService.ts
@@ -1,5 +1,5 @@
-import { EteCourseCompletionEventDto } from '../@types/shared'
-import { GetCourseCompletionRequest } from '../@types/user-defined'
+import { EteCourseCompletionEventDto, PagedModelEteCourseCompletionEventDto } from '../@types/shared'
+import { GetCourseCompletionRequest, GetCourseCompletionsRequest } from '../@types/user-defined'
 import CourseCompletionClient from '../data/courseCompletionClient'
 
 export default class CourseCompletionService {
@@ -7,5 +7,19 @@ export default class CourseCompletionService {
 
   async getCourseCompletion(request: GetCourseCompletionRequest): Promise<EteCourseCompletionEventDto> {
     return this.courseCourseCompletionClient.find(request)
+  }
+
+  async searchCourseCompletions({
+    username,
+    providerCode,
+    dateFrom,
+    dateTo,
+  }: GetCourseCompletionsRequest): Promise<PagedModelEteCourseCompletionEventDto> {
+    return this.courseCourseCompletionClient.getCourseCompletions({
+      username,
+      providerCode,
+      dateFrom,
+      dateTo,
+    })
   }
 }
