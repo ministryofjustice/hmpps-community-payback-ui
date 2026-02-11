8b54552d3ade0f29186bb9275f676245d6338ff8
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-02-11 10:52:18 +0000

    Add notification banner and error summary components to project view
    This will be used to display a success on update message.

--- server/controllers/projectsController.ts
+++ server/controllers/projectsController.ts
@@ -5,6 +5,7 @@ import ProviderService from '../services/providerService'
 import AppointmentService from '../services/appointmentService'
 import paths from '../paths'
 import getTeams from './shared/getTeams'
+import { generateErrorTextList } from '../utils/errorUtils'
 
 export default class ProjectsController {
   private readonly providerCode = 'N56'
@@ -38,11 +39,13 @@ export default class ProjectsController {
       const formattedProject = ProjectPage.projectDetails(project)
 
       const appointmentList = ProjectPage.appointmentList(appointments.content)
+      const errorList = generateErrorTextList(res.locals.errorMessages)
 
       res.render('projects/show', {
         project: formattedProject,
         appointmentList,
         backPath: paths.projects.index.pattern,
+        errorList,
       })
     }
   }
