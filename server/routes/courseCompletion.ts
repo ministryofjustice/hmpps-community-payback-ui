14f2aa63f905290262ca20a3b342fa106fc0a1f5
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-03-06 16:04:47 +0000

    Add route handling for course completions
    Thanks to the path map for the forms, we only need to add one route for show and submit.
    
    TODO: add audit log for the attempt to process the course completion

--- server/routes/courseCompletion.ts
+++ server/routes/courseCompletion.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 import paths from '../paths'
 import AuditService, { Page } from '../services/auditService'
-import CourseCompletionsController from '../controllers/courseCompletions'
+import { Controllers } from '../controllers'
+import { CourseCompletionPage } from '../pages/courseCompletions/process/pathMap'
 
 export default function courseCompletionRoutes(
-  courseCompletionsController: CourseCompletionsController,
+  controllers: Controllers,
   router: Router,
   auditService: AuditService,
 ): Router {
+  const { courseCompletionsController, processCourseCompletionsController: processCourseCompletionsControllers } =
+    controllers
   router.get(paths.courseCompletions.index.pattern, async (req, res, next) => {
     await auditService.logPageView(Page.SHOW_COURSE_COMPLETIONS_SEARCH_PAGE, {
       who: res.locals.user.username,
@@ -36,6 +39,20 @@ export default function courseCompletionRoutes(
 
     const handler = courseCompletionsController.show()
     await handler(req, res, next)
+  })
+
+  router.get(paths.courseCompletions.process.pattern, async (req, res) => {
+    const page = req.params.page as CourseCompletionPage
+
+    const handler = processCourseCompletionsControllers[page].show()
+    await handler(req, res)
+  })
+
+  router.post(paths.courseCompletions.process.pattern, async (req, res) => {
+    const page = req.params.page as CourseCompletionPage
+
+    const handler = processCourseCompletionsControllers[page].submit()
+    await handler(req, res)
   })
 
   return router
