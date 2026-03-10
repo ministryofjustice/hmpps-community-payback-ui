0cdb2641c8312205f390e54824bfd5edfda53dc5
Dan Liburd <daniel.liburd@gmail.com>
2026-03-10 13:49:57 +0000

    Update courseCompletionFactory type to include the PDU
    As we are adding the PDU to the data model, but not yet displaying it on a
    page, filter out that value in the integration test.

--- server/testutils/factories/courseCompletionFactory.ts
+++ server/testutils/factories/courseCompletionFactory.ts
@@ -1,6 +1,7 @@
 import { Factory } from 'fishery'
 import { faker, fakerEN_GB as fakerEngb } from '@faker-js/faker'
 import { EteCourseCompletionEventDto } from '../../@types/shared'
+import communityCampusPduFactory from './communityCampusPduFactory'
 
 export default Factory.define<EteCourseCompletionEventDto>(() => ({
   id: faker.string.uuid(),
@@ -14,11 +15,12 @@ export default Factory.define<EteCourseCompletionEventDto>(() => ({
   courseType: faker.company.buzzPhrase(),
   provider: faker.company.name(),
   completionDate: faker.date.recent().toISOString(),
-  status: 'COMPLETED',
+  status: 'Passed',
   totalTimeMinutes: faker.number.int({ min: 100, max: 200 }),
   expectedTimeMinutes: faker.number.int({ min: 100, max: 200 }),
   attempts: faker.number.int({ min: 1, max: 3 }),
   externalReference: faker.string.alphanumeric(),
   importedOn: faker.date.recent().toISOString(),
   resolved: faker.datatype.boolean(),
+  pdu: communityCampusPduFactory.build(),
 }))
