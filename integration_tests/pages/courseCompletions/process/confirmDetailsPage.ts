ab5f3fa00205ae289af06a6573774b8d4d16fd7e
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-03-11 11:50:52 +0000

    Add integration tests for confirm details page

---
+++ integration_tests/pages/courseCompletions/process/confirmDetailsPage.ts
@@ -0,0 +1,40 @@
+import { EteCourseCompletionEventDto } from '../../../../server/@types/shared'
+import paths from '../../../../server/paths'
+import SummaryListComponent from '../../components/summaryListComponent'
+import { pathWithQuery } from '../../../../server/utils/utils'
+import RadioGroupComponent from '../../components/radioGroupComponent'
+import { CourseCompletionForm } from '../../../../server/services/forms/courseCompletionFormService'
+import BaseCourseCompletionsPage from './baseCourseCompletionsPage'
+
+export default class ConfirmDetailsPage extends BaseCourseCompletionsPage {
+  private readonly formDetails: SummaryListComponent
+
+  readonly alertPractitionerQuestion: RadioGroupComponent
+
+  constructor(private readonly form: CourseCompletionForm) {
+    super()
+    this.formDetails = new SummaryListComponent()
+    this.alertPractitionerQuestion = new RadioGroupComponent('alertPractitioner')
+  }
+
+  static visit(courseCompletion: EteCourseCompletionEventDto, form: CourseCompletionForm) {
+    const path = pathWithQuery(paths.courseCompletions.process({ page: 'confirm', id: courseCompletion.id }), {
+      form: '1',
+    })
+    cy.visit(path)
+
+    return new ConfirmDetailsPage(form)
+  }
+
+  shouldShowCompletedDetails(): void {
+    this.formDetails.getValueWithLabel('CRN').should('contain.text', this.form.crn)
+  }
+
+  protected override customCheckOnPage(): void {
+    cy.get('h2').first().should('have.text', 'Confirm details')
+  }
+
+  clickChange(label: string) {
+    this.formDetails.clickActionWithLabel(label)
+  }
+}
