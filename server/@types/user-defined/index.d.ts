0feaee07f27ab8ecdcff6e8c618ddd761c5f8c21
Dan Liburd <daniel.liburd@gmail.com>
2026-03-12 13:13:44 +0000

    Introduce getProvidersAndPdus
    When searching for a course completion, we'll be using the PDU which is
    filtered by the region (provider).
    
    To ensure that this interaction is progressively enhanced, getProvidersAndPdus
    caters for multiple scenarios:
    
    - Loading the initial page where providerCode === null and pduId === null
    - selecting a provider where providerCode exists and pudId === null
    - selecting a PDU where providerCode exists and pduId exists

--- server/@types/user-defined/index.d.ts
+++ server/@types/user-defined/index.d.ts
@@ -1,5 +1,6 @@
 import type { Response } from 'express'
 import { AttendanceDataDto, ContactOutcomeDto, SupervisorSummaryDto, ProjectTypeDto } from '../shared'
+import ReferenceDataService from '../../services/referenceDataService'
 
 export interface AppointmentUpdatePageViewData {
   backLink: string
@@ -140,6 +141,14 @@ export type GetProvidersAndTeamsParams = {
   teamCode?: string
   response: Response
   providerService: ProviderService
+}
+
+export type GetProvidersAndPdusParams = {
+  providerCode?: string
+  pduId?: string
+  response: Response
+  providerService: ProviderService
+  referenceDataService: ReferenceDataService
 }
 
 export type SortDirection = 'asc' | 'desc'
---
+++ server/controllers/shared/getProvidersAndPdus.ts
@@ -0,0 +1,46 @@
+import GovUkSelectInput from '../../forms/GovUkSelectInput'
+import { GetProvidersAndPdusParams, GovUkSelectOption } from '../../@types/user-defined'
+
+export type ProvidersAndPdus = {
+  provider?: { text?: string; value: string }
+  providerItems?: Array<GovUkSelectOption>
+  pduItems: Array<GovUkSelectOption>
+}
+
+export default async ({
+  providerService,
+  providerCode,
+  referenceDataService,
+  pduId,
+  response,
+}: GetProvidersAndPdusParams): Promise<ProvidersAndPdus> => {
+  const providers = await providerService.getProviders(response.locals.user.username)
+  const pdus = await referenceDataService.getCommunityCampusPdus(response.locals.user.username)
+
+  const defaultProviderOptionText = 'Choose region'
+  const defaultPduOptionText = 'Choose PDU'
+
+  if (providers.length === 1) {
+    const [dto] = providers
+
+    const provider = { text: dto.name, value: dto.code }
+    const providerPdus = pdus.pdus.filter(pdu => pdu.providerCode === provider.value)
+    const pduItems = GovUkSelectInput.getOptions(providerPdus, 'name', 'id', defaultPduOptionText, pduId)
+
+    return { pduItems, provider }
+  }
+
+  const provider = providerCode ? { value: providerCode } : undefined
+  const providerItems = GovUkSelectInput.getOptions(providers, 'name', 'code', defaultProviderOptionText, providerCode)
+
+  if (!provider) {
+    const pduItems = GovUkSelectInput.getOptions(pdus.pdus, 'name', 'id', defaultProviderOptionText, pduId)
+
+    return { pduItems, provider, providerItems }
+  }
+
+  const providerPdus = pdus.pdus.filter(pdu => pdu.providerCode === provider.value)
+  const pduItems = GovUkSelectInput.getOptions(providerPdus, 'name', 'id', defaultPduOptionText, pduId)
+
+  return { pduItems, provider, providerItems }
+}
