155a7c60d7528965b818f62556a329252164d5d7
Dan Liburd <daniel.liburd@gmail.com>
2026-03-12 13:13:44 +0000

    Introduce getProvidersAndPdus
    When searching for a course completion, we'll be using the PDU which is
    filtered by the region (provider).
    
    To ensure that this interaction is progressively enhanced, getProvidersAndPdus
    caters for multiple scenarios:
    
    - Loading the initial page where providerCode === null and pduId === null
    - selecting a provider where providerCode exists and pudId === null
    - selecting a PDU where providerCode exists and pduId exists

---
+++ server/controllers/shared/getProvidersAndPdus.ts
@@ -0,0 +1,43 @@
+import GovUkSelectInput from '../../forms/GovUkSelectInput'
+import { GetProvidersAndPdusParams, GovUkSelectOption } from '../../@types/user-defined'
+
+export type ProvidersAndPdus = {
+  provider?: { text?: string; value: string }
+  providerItems?: Array<GovUkSelectOption>
+  pduItems: Array<GovUkSelectOption>
+}
+
+export default async ({
+  providerService,
+  providerCode,
+  referenceDataService,
+  pduId,
+  response,
+}: GetProvidersAndPdusParams): Promise<ProvidersAndPdus> => {
+  const providers = await providerService.getProviders(response.locals.user.username)
+  const pdus = await referenceDataService.getCommunityCampusPdus(response.locals.user.username)
+
+  const defaultProviderOptionText = 'Choose region'
+  let defaultPduOptionText = 'Select a PDU'
+
+  if (providers.length === 1) {
+    const [dto] = providers
+
+    const provider = { text: dto.name, value: dto.code }
+    const providerPdus = pdus.pdus.filter(pdu => pdu.providerCode === provider.value)
+    const pduItems = GovUkSelectInput.getOptions(providerPdus, 'name', 'id', defaultPduOptionText, pduId)
+
+    return { pduItems, provider }
+  }
+
+  const provider = providerCode ? { value: providerCode } : undefined
+  const providerItems = GovUkSelectInput.getOptions(providers, 'name', 'code', defaultProviderOptionText, providerCode)
+
+  if (!provider) {
+    defaultPduOptionText = defaultProviderOptionText
+  }
+
+  const pduItems = GovUkSelectInput.getOptions(pdus.pdus, 'name', 'id', defaultPduOptionText, pduId)
+
+  return { pduItems, provider, providerItems }
+}
