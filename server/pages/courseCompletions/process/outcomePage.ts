3442e6b5036fb42078feb87a45f8938c2176831a
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-03-20 11:47:24 +0000

    Add validation logic for time credited inputs
    We only require one of either inputs to have a value, as 30 minutes or 1 hour
    are both valid inputs.

--- server/pages/courseCompletions/process/outcomePage.ts
+++ server/pages/courseCompletions/process/outcomePage.ts
@@ -1,4 +1,6 @@
+import { ValidationErrors } from '../../../@types/user-defined'
 import { CourseCompletionForm } from '../../../services/forms/courseCompletionFormService'
+import { isWholePositiveNumber } from '../../../utils/utils'
 import BaseCourseCompletionFormPage from './baseCourseCompletionFormPage'
 import { CourseCompletionPage } from './pathMap'
 
@@ -24,7 +26,30 @@ export default class OutcomePage extends BaseCourseCompletionFormPage<Body> {
     return formData
   }
 
-  protected getValidationErrors(_: Body) {
-    return {}
+  protected getValidationErrors(body: Body) {
+    return this.getTimeErrors(body)
+  }
+
+  private getTimeErrors(body: Body) {
+    const validationErrors = {} as ValidationErrors<Body>
+
+    if (!body.hours && !body.minutes) {
+      validationErrors.hours = { text: 'Enter hours and minutes for credited hours' }
+      return validationErrors
+    }
+
+    if (body.hours) {
+      if (!isWholePositiveNumber(body.hours)) {
+        validationErrors.hours = { text: 'Enter valid hours for credited hours, for example 2' }
+      }
+    }
+
+    if (body.minutes) {
+      if (!isWholePositiveNumber(body.minutes) || Number(body.minutes) > 59) {
+        validationErrors.minutes = { text: 'Enter valid minutes for credited hours, for example 30' }
+      }
+    }
+
+    return validationErrors
   }
 }
