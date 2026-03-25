f3a4bdc7a8329189d816c97343679fadbc15be2c
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-03-25 13:01:23 +0000

    Display requirement details as summary card
    Adding a partial to keep the template easier to follow (and as we have partials
    for other cards).

--- integration_tests/pages/courseCompletions/process/outcomePage.ts
+++ integration_tests/pages/courseCompletions/process/outcomePage.ts
@@ -1,18 +1,23 @@
-import { EteCourseCompletionEventDto } from '../../../../server/@types/shared'
+import { EteCourseCompletionEventDto, UnpaidWorkDetailsDto } from '../../../../server/@types/shared'
 import paths from '../../../../server/paths'
 import { pathWithQuery } from '../../../../server/utils/utils'
 import CourseDetailsComponent from '../courseDetailsComponent'
 import NotesQuestionComponent from '../../components/notesQuestionComponent'
 import BaseCourseCompletionsPage from './baseCourseCompletionsPage'
+import SummaryListComponent from '../../components/summaryListComponent'
+import DateTimeFormats from '../../../../server/utils/dateTimeUtils'
 
 export default class OutcomePage extends BaseCourseCompletionsPage {
   readonly notesQuestions = new NotesQuestionComponent()
 
   readonly courseDetails: CourseDetailsComponent
 
+  private readonly requirementDetails: SummaryListComponent
+
   constructor(private readonly courseCompletion: EteCourseCompletionEventDto) {
     super('Record an outcome')
     this.courseDetails = new CourseDetailsComponent(this.courseCompletion)
+    this.requirementDetails = new SummaryListComponent('Unpaid work requirement')
   }
 
   static visit(courseCompletion: EteCourseCompletionEventDto) {
@@ -38,5 +43,21 @@ export default class OutcomePage extends BaseCourseCompletionsPage {
   shouldShowErrors() {
     this.shouldShowErrorSummary('hours', 'Enter hours and minutes for credited hours')
     this.shouldShowErrorSummary('date-day', 'Appointment date must include a day, month and year')
+  }
+
+  shouldShowRequirementDetails(upwDetail: UnpaidWorkDetailsDto) {
+    const totalHoursOrdered = DateTimeFormats.totalMinutesToHumanReadableHoursAndMinutes(upwDetail.requiredMinutes)
+    const totalHoursRemaining = DateTimeFormats.totalMinutesToHumanReadableHoursAndMinutes(
+      upwDetail.requiredMinutes - upwDetail.completedMinutes,
+    )
+    const maximumEteHours = DateTimeFormats.totalMinutesToHumanReadableHoursAndMinutes(upwDetail.allowedEteMinutes)
+    const eteHoursCredited = DateTimeFormats.totalMinutesToHumanReadableHoursAndMinutes(upwDetail.completedEteMinutes)
+    const eteHoursRemaining = DateTimeFormats.totalMinutesToHumanReadableHoursAndMinutes(upwDetail.remainingEteMinutes)
+
+    this.requirementDetails.getValueWithLabel('Total hours ordered').should('contain.text', totalHoursOrdered)
+    this.requirementDetails.getValueWithLabel('Total hours remaining').should('contain.text', totalHoursRemaining)
+    this.requirementDetails.getValueWithLabel('Maximum ETE hours').should('contain.text', maximumEteHours)
+    this.requirementDetails.getValueWithLabel('ETE time credited').should('contain.text', eteHoursCredited)
+    this.requirementDetails.getValueWithLabel('ETE time remaining').should('contain.text', eteHoursRemaining)
   }
 }
