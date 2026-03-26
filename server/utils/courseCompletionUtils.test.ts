c7a2113b88780d525f04fff153417699ef7fa856
Dan Liburd <daniel.liburd@gmail.com>
2026-03-26 12:41:03 +0000

    Add formattedOffenderDetails method to course completion utils

--- server/utils/courseCompletionUtils.test.ts
+++ server/utils/courseCompletionUtils.test.ts
@@ -1,4 +1,6 @@
 import courseCompletionFactory from '../testutils/factories/courseCompletionFactory'
+import offenderFullFactory from '../testutils/factories/offenderFullFactory'
+import offenderLimitedFactory from '../testutils/factories/offenderLimitedFactory'
 import CourseCompletionUtils from './courseCompletionUtils'
 import DateTimeFormats from './dateTimeUtils'
 
@@ -47,6 +49,36 @@ describe('CourseCompletionUtils', () => {
       })
       expect(DateTimeFormats.totalMinutesToHumanReadableHoursAndMinutes).toHaveBeenCalledWith(150)
       expect(DateTimeFormats.totalMinutesToHumanReadableHoursAndMinutes).toHaveBeenCalledWith(180)
+    })
+  })
+  describe('formattedOffenderDetails', () => {
+    describe('when the offender is limited', () => {
+      it('returns limited formatted offender details', () => {
+        const offender = offenderLimitedFactory.build()
+        const result = CourseCompletionUtils.formattedOffenderDetails(offender)
+        expect(result).toEqual({
+          crn: offender.crn,
+          isLimited: true,
+        })
+      })
+    })
+
+    describe('when the offender is not limited', () => {
+      it('returns full formatted offender details', () => {
+        const dateOfBirth = '10 March 1980'
+        jest.spyOn(DateTimeFormats, 'isoDateToUIDate').mockReturnValue(dateOfBirth)
+
+        const offender = offenderFullFactory.build()
+        const result = CourseCompletionUtils.formattedOffenderDetails(offender)
+
+        expect(result).toEqual({
+          crn: offender.crn,
+          isLimited: false,
+          firstName: offender.forename,
+          lastName: offender.surname,
+          dateOfBirth,
+        })
+      })
     })
   })
 })
