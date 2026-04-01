e514f5636475148ce7957551f6bf3b2006576762
tobybatchmoj <toby.batch@digital.justice.gov.uk>
2026-04-01 08:24:33 +0100

    Update nunjucksSetup.ts

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -17,7 +17,7 @@ export default function nunjucksSetup(app: express.Express): void {
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'HMPPS Community'
+  app.locals.applicationName = 'HMPPS Community Payback'
   app.locals.environmentName = config.environmentName
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
   app.locals.supportEmailAddress = config.supportEmailAddress
