fda60bb010c2b626546e25d9a49944b37c5fc437
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-04-01 11:39:52 +0100

    Add e2e test component for hours and minutes input

--- e2e-tests/pages/appointments/logHoursPage.ts
+++ e2e-tests/pages/appointments/logHoursPage.ts
@@ -1,25 +1,22 @@
 import { Locator, Page } from '@playwright/test'
 import AppointmentFormPage from './appointmentFormPage'
+import HoursMinutesInputComponent from '../components/hoursMinutesInputComponent'
 
 export default class LogHoursPage extends AppointmentFormPage {
   startTimeFieldLocator: Locator
 
   endTimeFieldLocator: Locator
 
-  penaltyTimeHoursFieldLocator: Locator
-
-  penaltyTimeMinutesFieldLocator: Locator
+  hoursMinutesInput: HoursMinutesInputComponent
 
   constructor(page: Page) {
     super(page, 'Log start and end time')
     this.startTimeFieldLocator = page.getByLabel('Start time')
     this.endTimeFieldLocator = page.getByLabel('End time')
-    this.penaltyTimeHoursFieldLocator = page.getByLabel('Hours')
-    this.penaltyTimeMinutesFieldLocator = page.getByLabel('Minutes')
+    this.hoursMinutesInput = new HoursMinutesInputComponent(page)
   }
 
-  async enterPenaltyHours() {
-    await this.penaltyTimeHoursFieldLocator.fill('1')
-    await this.penaltyTimeMinutesFieldLocator.fill('00')
+  async enterPenaltyHours(hours = '1', minutes = '00') {
+    await this.hoursMinutesInput.enterHours(hours, minutes)
   }
 }
