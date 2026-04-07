ff3d20d8d140b573746cd5bc643a521f72fc3f02
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-04-07 18:49:32 +0100

    Add integration tests for travel time form

---
+++ integration_tests/pages/appointments/updateTravelTimePage.ts
@@ -0,0 +1,25 @@
+import { AppointmentDto } from '../../../server/@types/shared'
+import Offender from '../../../server/models/offender'
+import paths from '../../../server/paths'
+import HoursMinutesInputComponent from '../components/hoursMinutesInputComponent'
+import Page from '../page'
+
+export default class UpdateTravelTimePage extends Page {
+  readonly timeInput = new HoursMinutesInputComponent()
+
+  constructor(appointment: AppointmentDto) {
+    const offender = new Offender(appointment.offender)
+    super(offender.name)
+  }
+
+  static visit(appointment: AppointmentDto): UpdateTravelTimePage {
+    const path = paths.appointments.travelTime.update({
+      projectCode: appointment.projectCode,
+      appointmentId: appointment.id.toString(),
+    })
+
+    cy.visit(path)
+
+    return new UpdateTravelTimePage(appointment)
+  }
+}
---
+++ integration_tests/tests/appointments/adjustTravelTime.cy.ts
@@ -0,0 +1,58 @@
+//  Feature: Adjust travel time for an appointment
+//    As a case administrator
+//    I want to adjust travel time for an appointment
+//    So that I can correctly track all time completed for an unpaid work order
+
+//  Scenario: Updating travel time
+//    Given I am on the adjust travel time page for an appointment
+//    When I complete the form
+//    Then I see the travel time dashboard
+
+//  Scenario: Validating input
+//    Given I am on the adjust travel time page for an appointment
+//    When I do not complete the form
+//    And I click submit
+//    Then I should see the page with errors
+
+import appointmentFactory from '../../../server/testutils/factories/appointmentFactory'
+import SearchAttendedPage from '../../pages/appointments/searchAttendedPage'
+import UpdateTravelTimePage from '../../pages/appointments/updateTravelTimePage'
+import Page from '../../pages/page'
+
+context('Update travel time page', () => {
+  const appointment = appointmentFactory.build()
+
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.signIn()
+    cy.task('stubFindAppointment', { appointment })
+  })
+
+  // Scenario: Updating travel time
+  it('submits travel time and returns to dashboard', () => {
+    // Given I am on the adjust travel time page for an appointment
+    const page = UpdateTravelTimePage.visit(appointment)
+
+    //  When I complete the form
+    page.timeInput.enterTime()
+    page.clickSubmit()
+
+    // Then I see the travel time dashboard
+    Page.verifyOnPage(SearchAttendedPage)
+  })
+
+  // Scenario: Validating input
+  it('shows validation errors', () => {
+    // Given I am on the adjust travel time page for an appointment
+    const page = UpdateTravelTimePage.visit(appointment)
+
+    // When I do not complete the form
+    // And I click submit
+    page.clickSubmit()
+
+    // Then I should see the page with errors
+    page.checkOnPage()
+    page.timeInput.shouldShowMissingValueError()
+  })
+})
