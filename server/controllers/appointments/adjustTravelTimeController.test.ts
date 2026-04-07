68491aa99839a7923e72ed35a39561686c31f7a8
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-04-07 17:18:08 +0100

    Add controller methods for travel time page
    This handles rendering the page with basic view data (offender details and paths), validation
    of user input and redirection if no errors. Page content and submit of travel
    time input to be added separately.

---
+++ server/controllers/appointments/adjustTravelTimeController.test.ts
@@ -0,0 +1,79 @@
+import { createMock } from '@golevelup/ts-jest'
+import type { NextFunction, Request, Response } from 'express'
+import UpdateTravelTimePage from '../../pages/appointments/updateTravelTimePage'
+import AdjustTravelTimeController from './adjustTravelTimeController'
+import paths from '../../paths'
+import AppointmentService from '../../services/appointmentService'
+import Offender from '../../models/offender'
+
+describe('AdjustTravelTimeController', () => {
+  const templatePath = 'appointments/update/travelTime/update'
+  const page = createMock<UpdateTravelTimePage>()
+  const appointmentService = createMock<AppointmentService>()
+  const response = createMock<Response>()
+  const next = createMock<NextFunction>({})
+  let controller: AdjustTravelTimeController
+  const viewData = {
+    offender: { crn: '1234', name: 'Sam Smith', isLimited: false } as Offender,
+    backLink: '/back',
+    updatePath: '/update',
+  }
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+    controller = new AdjustTravelTimeController(page, appointmentService)
+    page.viewData.mockReturnValue(viewData)
+  })
+
+  describe('update', () => {
+    it('should render the page', async () => {
+      const appointmentId = '1'
+      const projectCode = '2'
+      const request = createMock<Request>({ params: { appointmentId, projectCode } })
+
+      const requestHandler = controller.update()
+      await requestHandler(request, response, next)
+
+      expect(response.render).toHaveBeenCalledWith(templatePath, viewData)
+    })
+  })
+
+  describe('submitUpdate', () => {
+    describe('no errors', () => {
+      it('redirects to the next page', async () => {
+        page.validationErrors.mockReturnValue({ hasErrors: false, errors: {}, errorSummary: [] })
+
+        const request = createMock<Request>({ params: { id: '1' }, query: {} })
+
+        const requestHandler = controller.submitUpdate()
+        await requestHandler(request, response, next)
+
+        expect(response.redirect).toHaveBeenCalledWith(paths.appointments.adjustTravelTime({}))
+      })
+    })
+    describe('has errors', () => {
+      it('rerenders page if validation errors', async () => {
+        const errorSummary = [{ text: 'Error 1', href: '#1', attributes: { 'some-attr': 'value' } }]
+        const errors = { hours: { text: 'Error' } }
+        page.validationErrors.mockReturnValue({ hasErrors: true, errors, errorSummary })
+
+        const appointmentId = '1'
+        const projectCode = '2'
+        const body = { hours: 't', minutes: 'r' }
+        const request = createMock<Request>({ params: { appointmentId, projectCode }, body })
+
+        const requestHandler = controller.submitUpdate()
+        await requestHandler(request, response, next)
+
+        expect(response.render).toHaveBeenCalledWith(templatePath, {
+          ...viewData,
+          errors,
+          errorSummary,
+          time: body,
+        })
+
+        expect(page.validationErrors).toHaveBeenCalledWith(body)
+      })
+    })
+  })
+})
--- server/controllers/appointments/adjustTravelTimeController.ts
+++ server/controllers/appointments/adjustTravelTimeController.ts
@@ -1,9 +1,63 @@
 import type { Request, RequestHandler, Response } from 'express'
+import paths from '../../paths'
+import UpdateTravelTimePage from '../../pages/appointments/updateTravelTimePage'
+import AppointmentService from '../../services/appointmentService'
 
 export default class AdjustTravelTimeController {
+  constructor(
+    private readonly page: UpdateTravelTimePage,
+    private readonly appointmentService: AppointmentService,
+  ) {}
+
   show(): RequestHandler {
     return async (_req: Request, res: Response) => {
       res.render('appointments/update/adjustTravelTime')
+    }
+  }
+
+  update(): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { projectCode, appointmentId } = req.params
+      const appointment = await this.appointmentService.getAppointment({
+        projectCode,
+        appointmentId,
+        username: res.locals.user.username,
+      })
+
+      const viewData = this.page.viewData(appointment)
+
+      res.render('appointments/update/travelTime/update', viewData)
+    }
+  }
+
+  submitUpdate(): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { projectCode, appointmentId } = req.params
+      const { hasErrors, errorSummary, errors } = this.page.validationErrors(req.body)
+
+      if (hasErrors) {
+        const { hours, minutes } = req.body
+        const time = {
+          hours,
+          minutes,
+        }
+
+        const appointment = await this.appointmentService.getAppointment({
+          projectCode,
+          appointmentId,
+          username: res.locals.user.username,
+        })
+
+        const viewData = {
+          ...this.page.viewData(appointment),
+          errorSummary,
+          errors,
+          time,
+        }
+
+        return res.render('appointments/update/travelTime/update', viewData)
+      }
+      return res.redirect(paths.appointments.adjustTravelTime({}))
     }
   }
 }
