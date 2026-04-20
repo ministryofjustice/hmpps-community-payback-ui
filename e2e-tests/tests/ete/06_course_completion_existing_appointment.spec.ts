eacea52092ef670bb53f2f1e20fb6bb718bbf018
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-04-20 11:18:33 +0100

    Add test for course completion recorded against existing appointment
    Also rename existing appointment to reflect that it is testing creating a new
    appointment.

---
+++ e2e-tests/tests/ete/06_course_completion_existing_appointment.spec.ts
@@ -0,0 +1,66 @@
+import test from '../../fixtures/test'
+import CourseCompletionDetailsPage from '../../pages/courseCompletions/courseCompletionDetailsPage'
+import CourseCompletionFormPage from '../../pages/courseCompletions/courseCompletionFormPage'
+import searchCourseCompletions from '../../steps/searchCourseCompletions'
+import sendCourseCompletionMessage from '../../steps/sendCourseCompletionMessage'
+import signIn from '../../steps/signIn'
+
+test('Process course completion - credit time on existing appointment', async ({
+  eteExternalApiClient,
+  page,
+  deliusUser,
+  team,
+  personOnProbation,
+  project,
+  appointment,
+}) => {
+  await test.step('Send Course Completion Message', async () => {
+    return sendCourseCompletionMessage(eteExternalApiClient, team, personOnProbation)
+  })
+
+  const homePage = await signIn(page, deliusUser)
+  const searchCourseCompletionsPage = await searchCourseCompletions(page, homePage, team)
+
+  await searchCourseCompletionsPage.expect.toSeeCourseCompletions()
+
+  const personName = personOnProbation.getFullName()
+
+  await searchCourseCompletionsPage.clickCourseCompletion(personName)
+
+  const courseCompletionsDetailsPage = new CourseCompletionDetailsPage(page, personName)
+  await courseCompletionsDetailsPage.expect.toBeOnThePage()
+  await courseCompletionsDetailsPage.clickProcess()
+
+  const courseCompletionFormPage = new CourseCompletionFormPage(page, true)
+
+  await courseCompletionFormPage.expect.toBeOnThePage('crn')
+  await courseCompletionFormPage.fillCrn(personOnProbation.crn)
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('person')
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('history')
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('requirement')
+  await courseCompletionFormPage.selectRequirement()
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('project')
+  await courseCompletionFormPage.selectProject(team, project)
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('appointments')
+  await courseCompletionFormPage.selectAppointment(appointment)
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('outcome')
+  await courseCompletionFormPage.completeOutcomeForm()
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('confirm')
+  await courseCompletionFormPage.continue()
+
+  await searchCourseCompletionsPage.expect.toBeOnThePage()
+})
