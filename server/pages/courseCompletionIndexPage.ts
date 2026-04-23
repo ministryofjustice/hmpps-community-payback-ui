b3cbe23ceaab7c35b1bfafe393956686e7fac55f
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-04-23 16:48:36 +0100

    Remove date related properties and methods page model
    The course completion search no longer has a date range, so these methods and
    properrties are not in use.
    
    The pdu property is missing from the query interface.

--- server/pages/courseCompletionIndexPage.ts
+++ server/pages/courseCompletionIndexPage.ts
@@ -1,24 +1,13 @@
 import { EteCourseCompletionEventDto } from '../@types/shared'
 import { CourseCompletionSortField, SortDirection, TableCell, ValidationErrors } from '../@types/user-defined'
-import GovukFrontendDateInput from '../forms/GovukFrontendDateInput'
 import paths from '../paths'
 import DateTimeFormats from '../utils/dateTimeUtils'
 import HtmlUtils from '../utils/htmlUtils'
 import sortHeader from '../utils/sortHeader'
 
-type DateFields = 'startDate' | 'endDate'
-type TimePeriods = 'day' | 'month' | 'year'
-type DateKeys = `${DateFields}-${TimePeriods}`
-
 export type CourseCompletionPageInput = {
-  provider: string
-} & {
-  [K in DateKeys]: string
-}
-
-interface SearchValues {
-  dateFrom: string
-  dateTo: string
+  provider?: string
+  pdu?: string
 }
 
 export default class CourseCompletionIndexPage {
@@ -36,26 +25,6 @@ export default class CourseCompletionIndexPage {
     }
 
     return errors
-  }
-
-  items() {
-    return {
-      startDateItems: GovukFrontendDateInput.getDateItems(this.query, 'startDate', false),
-      endDateItems: GovukFrontendDateInput.getDateItems(this.query, 'endDate', false),
-    }
-  }
-
-  searchValues(): SearchValues {
-    return {
-      dateFrom: `${this.query['startDate-year']}-${this.query['startDate-month']}-${this.query['startDate-day']}`,
-      dateTo: `${this.query['endDate-year']}-${this.query['endDate-month']}-${this.query['endDate-day']}`,
-    }
-  }
-
-  dateFields(): Partial<CourseCompletionPageInput> {
-    return Object.fromEntries(
-      Object.entries(this.query).filter(([key]) => key.includes('startDate-') || key.includes('endDate-')),
-    )
   }
 
   courseCompletionTableHeaders(
