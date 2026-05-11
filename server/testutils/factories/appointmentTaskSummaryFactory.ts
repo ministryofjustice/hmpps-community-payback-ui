4a048aa8aeee16921e89462b069c2258c63963c2
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-05-11 15:42:34 +0100

    Add required properties to factories

--- server/testutils/factories/appointmentTaskSummaryFactory.ts
+++ server/testutils/factories/appointmentTaskSummaryFactory.ts
@@ -2,8 +2,10 @@ import { Factory } from 'fishery'
 import { faker } from '@faker-js/faker'
 import { AppointmentTaskSummaryDto } from '../../@types/shared'
 import appointmentSummaryFactory from './appointmentSummaryFactory'
+import offenderFullFactory from './offenderFullFactory'
 
 export default Factory.define<AppointmentTaskSummaryDto>(() => ({
   taskId: faker.string.alphanumeric(),
   appointment: appointmentSummaryFactory.build(),
+  offender: offenderFullFactory.build(),
 }))
