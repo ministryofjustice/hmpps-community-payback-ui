12ef2e01fa0ce884fe83ae460d76c42836bc39d7
Dan Liburd <daniel.liburd@gmail.com>
2026-05-13 15:50:37 +0100

    Add e2e test step for verifying travel time

--- e2e-tests/steps/creditTravelTime.ts
+++ e2e-tests/steps/creditTravelTime.ts
@@ -2,8 +2,12 @@ import { Page } from '@playwright/test'
 import TravelTimePage from '../pages/travelTime/travelTimePage'
 import SearchTravelTimePage from '../pages/travelTime/searchTravelTimePage'
 
-export default async (page: Page, travelTimePage: TravelTimePage) => {
-  await travelTimePage.completeTravelTimeForm()
+export default async (
+  page: Page,
+  travelTimePage: TravelTimePage,
+  timeCredited: { hours: string; minutes: string } = { hours: '1', minutes: '10' },
+) => {
+  await travelTimePage.completeTravelTimeForm(timeCredited)
 
   await travelTimePage.submitCreditTravelTime()
 
