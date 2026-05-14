c7769bf393c70c72452022b6859bda101bfe42e5
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-05-14 16:57:26 +0100

    Move call to action for appointment details and update wording
    As this page isn't tecnically a form, we override a new form block, and instead
    add a better worded call to action to 'Update' the appointment in the header.

--- e2e-tests/pages/appointments/checkAppointmentDetailsPage.ts
+++ e2e-tests/pages/appointments/checkAppointmentDetailsPage.ts
@@ -4,12 +4,19 @@ import AppointmentFormPage from './appointmentFormPage'
 export default class CheckAppointmentDetailsPage extends AppointmentFormPage {
   readonly supervisorInputLocator: Locator
 
+  readonly updateButtonLocator: Locator
+
   constructor(page: Page) {
     super(page, 'Appointment details')
     this.supervisorInputLocator = page.getByLabel('Choose supervisor')
+    this.updateButtonLocator = page.getByRole('button', { name: 'Update appointment' })
   }
 
   chooseSupervisor(supervisor: string) {
     this.supervisorInputLocator.selectOption({ label: supervisor })
+  }
+
+  override async continue() {
+    await this.updateButtonLocator.click()
   }
 }
