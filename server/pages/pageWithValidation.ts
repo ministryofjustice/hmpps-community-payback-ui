997e322dd509c065297ca9a508e5de61b9e21913
Dan Liburd <daniel.liburd@gmail.com>
2026-05-26 17:31:42 +0100

    Allow passing additional params to be passed to page validation method

--- server/pages/pageWithValidation.ts
+++ server/pages/pageWithValidation.ts
@@ -1,14 +1,14 @@
 import { ValidationErrors } from '../@types/user-defined'
 import { ErrorViewData, generateErrorSummary } from '../utils/errorUtils'
 
-export default abstract class PageWithValidation<TBody> {
-  validationErrors(query: TBody): ErrorViewData<TBody> {
-    const errors = this.getValidationErrors(query)
+export default abstract class PageWithValidation<TBody, TContext = unknown> {
+  validationErrors(query: TBody, additionalParams?: TContext): ErrorViewData<TBody> {
+    const errors = this.getValidationErrors(query, additionalParams)
 
     const errorSummary = generateErrorSummary(errors)
 
     return { errors, hasErrors: Object.keys(errors).length > 0, errorSummary }
   }
 
-  protected abstract getValidationErrors(query: TBody): ValidationErrors<TBody>
+  protected abstract getValidationErrors(query: TBody, additionalParams?: TContext): ValidationErrors<TBody>
 }
