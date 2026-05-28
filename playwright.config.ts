f2ca013b246ff83e9b861050fbec69d0084e62c3
Dan Liburd <daniel.liburd@gmail.com>
2026-05-28 12:14:58 +0100

    Increase e2e test workers to 4

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig<TestOptions>({
   forbidOnly: !!process.env.CI,
   retries: 1,
   maxFailures: 2,
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   reporter: [['html', { outputFolder: './e2e-tests/playwright-report/index.html' }]],
   timeout: process.env.CI ? 5 * 60 * 1000 : 2 * 60 * 1000,
   use: {
