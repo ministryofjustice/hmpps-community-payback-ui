c4860198659bbfdec97f3e07d3f6c22172b7fda4
Dan Liburd <daniel.liburd@gmail.com>
2026-06-02 15:43:26 +0100

    Add e2e test for processing a course completion failure

---
+++ e2e-tests/tests/ete/course_completion_process_failure.spec.ts
@@ -0,0 +1,81 @@
+import { login as deliusLogin } from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/login'
+import verifyTimeCredited from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/upw/verify-time-credited'
+import test from '../../fixtures/test'
+import CourseCompletionDetailsPage from '../../pages/courseCompletions/courseCompletionDetailsPage'
+import CourseCompletionFormPage from '../../pages/courseCompletions/courseCompletionFormPage'
+import searchCourseCompletions from '../../steps/searchCourseCompletions'
+import sendCourseCompletionMessage from '../../steps/sendCourseCompletionMessage'
+import signIn from '../../steps/signIn'
+
+test('Process course completion failure', async ({
+  eteExternalApiClient,
+  page,
+  deliusUser,
+  e2eProjects,
+  team,
+  personOnProbation,
+}) => {
+  await test.step('Send Course Completion Failure Message', async () => {
+    return sendCourseCompletionMessage(eteExternalApiClient, team, personOnProbation, 'Failed')
+  })
+
+  const homePage = await signIn(page, deliusUser)
+  await homePage.courseCompletionsLink.click()
+  const searchCourseCompletionsPage = await searchCourseCompletions(page, team)
+
+  await searchCourseCompletionsPage.expect.toSeeSearchResults()
+
+  const personName = personOnProbation.getFullName()
+
+  await searchCourseCompletionsPage.clickCourseCompletion(personName)
+
+  const courseCompletionsDetailsPage = new CourseCompletionDetailsPage(page, personName)
+  await courseCompletionsDetailsPage.expect.toBeOnThePage()
+  await courseCompletionsDetailsPage.clickProcess()
+
+  const courseCompletionFormPage = new CourseCompletionFormPage(page)
+
+  await courseCompletionFormPage.expect.toBeOnThePage('crn')
+  await courseCompletionFormPage.fillCrn(personOnProbation.crn)
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('person')
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('history')
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('requirement')
+  await courseCompletionFormPage.selectRequirement()
+  await courseCompletionFormPage.continue()
+
+  const [projectName] = e2eProjects
+  await courseCompletionFormPage.expect.toBeOnThePage('project')
+  await courseCompletionFormPage.selectProject(team, projectName)
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('appointments')
+  await courseCompletionFormPage.createNewAppointmentButton.click()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('outcome')
+  const timeCredited = { hours: '1', minutes: '10' }
+  const date = new Date()
+  await courseCompletionFormPage.completeOutcomeForm(timeCredited, date)
+  await courseCompletionFormPage.continue()
+
+  await courseCompletionFormPage.expect.toBeOnThePage('confirm')
+  await courseCompletionFormPage.continue()
+
+  await searchCourseCompletionsPage.expect.toBeOnThePage()
+  await searchCourseCompletionsPage.expect.toSeeSearchResults()
+  await searchCourseCompletionsPage.courseCompletions.expect.notToHaveRowWithContent(personName)
+
+  await deliusLogin(page)
+  await verifyTimeCredited(page, {
+    crn: personOnProbation.crn,
+    projectName,
+    hoursCredited: `${timeCredited.hours}:${timeCredited.minutes}`,
+    outcome: 'Attended - Complied',
+    date,
+  })
+})
