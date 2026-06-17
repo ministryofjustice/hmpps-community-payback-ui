8dd4e9d1620adc536e62198805d9e0f2a6d31f4b
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-06-17 14:42:14 +0100

    Add integration tests for bulk update select people page

--- integration_tests/pages/appointments/bulkUpdatePage.ts
+++ integration_tests/pages/appointments/bulkUpdatePage.ts
@@ -1,5 +1,7 @@
 import { AppointmentDto, SessionDto } from '../../../server/@types/shared'
 import DateTimeFormats from '../../../server/utils/dateTimeUtils'
+import { pathWithQuery } from '../../../server/utils/utils'
+import paths from '../../../server/paths'
 import RadioOrCheckboxGroupComponent from '../components/radioOrCheckboxGroupComponent'
 import Page from '../page'
 
@@ -11,6 +13,23 @@ export default class BulkUpdatePage extends Page {
     this.checkBoxes = new RadioOrCheckboxGroupComponent('appointments')
   }
 
+  static visitForSession(session: SessionDto, formId?: string): BulkUpdatePage {
+    const query = formId ? { form: formId } : undefined
+    const path = pathWithQuery(
+      paths.sessions.bulkUpdate({
+        projectCode: session.projectCode,
+        date: session.date,
+      }),
+      query,
+    )
+
+    return this.visitAndCheck(path, session)
+  }
+
+  selectPeople(appointments: Array<Pick<AppointmentDto, 'id'>>) {
+    appointments.forEach(appointment => this.checkBoxes.checkOptionWithValue(appointment.id.toString()))
+  }
+
   shouldShowSelectedPeople(appointments: Array<Pick<AppointmentDto, 'id'>>) {
     const selectedValues = appointments.map(appointment => appointment.id.toString())
     selectedValues.forEach(id => this.checkBoxes.shouldHaveSelectedValue(id))
@@ -19,6 +38,10 @@ export default class BulkUpdatePage extends Page {
   shouldShowNotSelectedPeople(appointments: Array<Pick<AppointmentDto, 'id'>>) {
     const selectedValues = appointments.map(appointment => appointment.id.toString())
     selectedValues.forEach(id => this.checkBoxes.shouldHaveUnselectedValue(id))
+  }
+
+  shouldNotHaveAnySelectedPeople() {
+    this.checkBoxes.shouldNotHaveASelectedValue()
   }
 
   protected override customCheckOnPage(): void {
---
+++ integration_tests/tests/group-sessions/bulk-update/selectPeople.cy.ts
@@ -0,0 +1,155 @@
+import appointmentFactory from '../../../../server/testutils/factories/appointmentFactory'
+import appointmentOutcomeFormFactory from '../../../../server/testutils/factories/appointmentOutcomeFormFactory'
+import appointmentSummaryFactory from '../../../../server/testutils/factories/appointmentSummaryFactory'
+import projectFactory from '../../../../server/testutils/factories/projectFactory'
+import providerSummaryFactory from '../../../../server/testutils/factories/providerSummaryFactory'
+import providerTeamSummaryFactory from '../../../../server/testutils/factories/providerTeamSummaryFactory'
+import sessionFactory from '../../../../server/testutils/factories/sessionFactory'
+import sessionSummaryFactory from '../../../../server/testutils/factories/sessionSummaryFactory'
+import BulkUpdatePage from '../../../pages/appointments/bulkUpdatePage'
+import ChooseSupervisorPage from '../../../pages/appointments/chooseSupervisorPage'
+import FindASessionPage from '../../../pages/findASessionPage'
+import Page from '../../../pages/page'
+import ViewSessionPage from '../../../pages/viewSessionPage'
+
+context('Group Session Bulk Update - Bulk Update', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.signIn()
+
+    const project = projectFactory.build()
+    cy.wrap(project).as('project')
+
+    const appointmentSummaryWithContactOutcome = appointmentSummaryFactory.build({
+      projectCode: project.projectCode,
+    })
+
+    const selectableAppointments = appointmentSummaryFactory.buildList(3, {
+      projectCode: project.projectCode,
+      contactOutcome: undefined,
+    })
+
+    const session = sessionFactory.build({
+      projectCode: project.projectCode,
+      appointmentSummaries: [...selectableAppointments, appointmentSummaryWithContactOutcome],
+    })
+    cy.wrap(session).as('session')
+
+    const selectedAppointments = selectableAppointments.slice(0, 2)
+    cy.wrap(selectedAppointments).as('selectedAppointments')
+
+    const unselectedAppointment = selectableAppointments[2]
+    cy.wrap(unselectedAppointment).as('unselectedAppointment')
+
+    const form = appointmentOutcomeFormFactory.build({
+      appointments: selectedAppointments.map(appointment => ({
+        id: appointment.id,
+        deliusVersion: '',
+      })),
+    })
+    cy.wrap(form).as('form')
+
+    cy.task('stubFindProject', { project })
+    cy.task('stubFindSession', { session })
+    cy.task('stubGetAppointmentForm', form)
+    cy.task('stubSaveAppointmentForm')
+  })
+
+  describe('visit with form query parameter', function describe() {
+    it('shows any selected appointments', function test() {
+      cy.task('stubGetAppointmentForm', this.form)
+
+      const page = BulkUpdatePage.visitForSession(this.session, '123')
+
+      page.shouldShowSelectedPeople(this.selectedAppointments)
+      page.shouldShowNotSelectedPeople([this.unselectedAppointment])
+    })
+  })
+
+  describe('visit without form query parameter', function describe() {
+    it('can select one or more appointments and continue to the choose supervisor page', function test() {
+      this.selectedAppointments.forEach(summary => {
+        const appointment = appointmentFactory.build({
+          id: summary.id,
+          projectCode: this.project.projectCode,
+          date: this.session.date,
+          offender: summary.offender,
+        })
+        cy.task('stubFindAppointment', { appointment })
+      })
+
+      const teams = providerTeamSummaryFactory.buildList(2)
+      cy.task('stubGetTeams', { teams: { providers: teams }, providerCode: this.project.providerCode })
+
+      const page = BulkUpdatePage.visitForSession(this.session)
+      page.selectPeople(this.selectedAppointments)
+      page.clickSubmit()
+
+      const supervisorPage = Page.verifyOnPage(ChooseSupervisorPage, this.session)
+      supervisorPage.selectedPeopleCard.shouldShowSelectedPeople(this.selectedAppointments)
+
+      supervisorPage.selectedPeopleCard.shouldNotShowPeople([this.unselectedAppointment])
+    })
+
+    it('shows validation errors if user attempts to continue without selecting an appointment', function test() {
+      cy.task('stubSaveAppointmentForm')
+      cy.task('stubGetAppointmentForm', appointmentOutcomeFormFactory.build({ appointments: [] }))
+      const page = BulkUpdatePage.visitForSession(this.session)
+      page.clickSubmit()
+
+      page.shouldShowErrorSummary('appointments', 'Select people with the same outcome')
+      page.shouldNotHaveAnySelectedPeople()
+    })
+  })
+
+  it('can navigate back to session list', function test() {
+    const provider = providerSummaryFactory.build()
+    const team = providerTeamSummaryFactory.build()
+
+    const form = appointmentOutcomeFormFactory.build({
+      originalSearch: {
+        provider: provider.code,
+        team: team.code,
+        'startDate-day': '18',
+        'startDate-month': '09',
+        'startDate-year': '2025',
+        'endDate-day': '20',
+        'endDate-month': '09',
+        'endDate-year': '2025',
+      },
+    })
+
+    const sessionSummary = sessionSummaryFactory.build({
+      projectCode: this.session.projectCode,
+      projectName: this.session.projectName,
+      date: this.session.date,
+    })
+
+    cy.task('stubGetAppointmentForm', form)
+    cy.task('stubGetProviders', { providers: { providers: [provider] } })
+    cy.task('stubGetTeams', { teams: { providers: [team] }, providerCode: provider.code })
+    cy.task('stubGetSessions', {
+      request: {
+        providerCode: provider.code,
+        teamCode: team.code,
+        startDate: '2025-09-18',
+        endDate: '2025-09-20',
+        username: 'some-name',
+      },
+      sessions: {
+        content: [sessionSummary],
+      },
+    })
+
+    const page = BulkUpdatePage.visitForSession(this.session, '123')
+    page.clickBack()
+
+    const viewSessionPage = Page.verifyOnPage(ViewSessionPage, this.session)
+    viewSessionPage.clickBack()
+
+    const findASessionPage = Page.verifyOnPage(FindASessionPage)
+    findASessionPage.shouldShowPopulatedSearchForm()
+    findASessionPage.shouldShowSearchResults(sessionSummary)
+  })
+})
