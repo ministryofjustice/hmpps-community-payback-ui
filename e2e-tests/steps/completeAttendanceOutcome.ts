19ecb96dee2e40895914bf9a3528870057a222ae
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-06-22 14:27:10 +0100

    Make check is sensitive optional in attendance outcome page step
    This question will not be visible on bulk update.

--- e2e-tests/steps/completeAttendanceOutcome.ts
+++ e2e-tests/steps/completeAttendanceOutcome.ts
@@ -2,18 +2,57 @@ import { Page } from '@playwright/test'
 import AttendanceOutcomePage from '../pages/appointments/attendanceOutcomePage'
 import LogHoursPage from '../pages/appointments/logHoursPage'
 
-export const completeAttendedCompliedOutcome = async (page: Page, attendanceOutcomePage: AttendanceOutcomePage) => {
-  return step(page, attendanceOutcomePage, () => attendanceOutcomePage.chooseAttendedCompliedOutcome())
+export const completeAttendedCompliedOutcome = async (
+  page: Page,
+  attendanceOutcomePage: AttendanceOutcomePage,
+  checkIsSensitive: boolean = false,
+) => {
+  return step(
+    page,
+    attendanceOutcomePage,
+    () => attendanceOutcomePage.chooseAttendedCompliedOutcome(),
+    checkIsSensitive,
+  )
 }
 
-export const completeAttendedEnforceableOutcome = async (page: Page, attendanceOutcomePage: AttendanceOutcomePage) => {
-  return step(page, attendanceOutcomePage, () => attendanceOutcomePage.chooseAttendedEnforceableOutcome())
+export const completeAttendedEnforceableOutcome = async (
+  page: Page,
+  attendanceOutcomePage: AttendanceOutcomePage,
+  checkIsSensitive: boolean = false,
+) => {
+  return step(
+    page,
+    attendanceOutcomePage,
+    () => attendanceOutcomePage.chooseAttendedEnforceableOutcome(),
+    checkIsSensitive,
+  )
 }
 
-const step = async (page: Page, attendanceOutcomePage: AttendanceOutcomePage, chooseOutcome: () => Promise<void>) => {
+export const completeNotAttendedOutcome = async (
+  page: Page,
+  attendanceOutcomePage: AttendanceOutcomePage,
+  checkIsSensitive: boolean = false,
+) =>
+  step(
+    page,
+    attendanceOutcomePage,
+    () => attendanceOutcomePage.chooseNotAttendedNotEnforcementOutcome(),
+    checkIsSensitive,
+  )
+
+const step = async (
+  page: Page,
+  attendanceOutcomePage: AttendanceOutcomePage,
+  chooseOutcome: () => Promise<void>,
+  checkIsSensitive: boolean,
+) => {
   const logHoursPage = new LogHoursPage(page)
 
   await chooseOutcome()
+
+  if (checkIsSensitive) {
+    await attendanceOutcomePage.isSensitiveOptionLocator.check()
+  }
 
   await attendanceOutcomePage.continue()
   await logHoursPage.expect.toBeOnThePage()
--- e2e-tests/tests/group-placements/01_update_appointment.spec.ts
+++ e2e-tests/tests/group-placements/01_update_appointment.spec.ts
@@ -30,7 +30,7 @@ test('Update a session appointment', async ({ page, deliusUser, team, project, p
 
   const attendanceOutcomePage = await completeChooseSupervisor(page, chooseSupervisorPage, team)
 
-  const logHoursPage = await completeAttendedCompliedOutcome(page, attendanceOutcomePage)
+  const logHoursPage = await completeAttendedCompliedOutcome(page, attendanceOutcomePage, true)
   await logHoursPage.enterPenaltyHours()
   await logHoursPage.continue()
 
