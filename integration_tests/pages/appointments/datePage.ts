c8a296632b32415f680b35ad7559e5c46f3b51b9
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-07-23 15:51:54 +0100

    Add integration tests for the date page
    I have omitted a back navigation test as this is not yet implemented fully

---
+++ integration_tests/pages/appointments/datePage.ts
@@ -0,0 +1,23 @@
+import BaseAppointmentFormPage, { AppointmentTitleContext } from './baseAppointmentFormPage'
+import { AppointmentFormPage } from '../../../server/pages/appointments/pathMap'
+
+export default class DatePage extends BaseAppointmentFormPage {
+  protected override page: AppointmentFormPage = 'date'
+
+  constructor(context: AppointmentTitleContext) {
+    super(context)
+  }
+
+  protected override customCheckOnPage(): void {
+    cy.get('label').should('contain.text', 'Add date')
+  }
+
+  clearDate() {
+    cy.get('#date').clear()
+  }
+
+  enterDate(date: string): void {
+    this.clearDate()
+    cy.get('#date').type(date)
+  }
+}
