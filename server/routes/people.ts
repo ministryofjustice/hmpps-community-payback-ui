a344f76d3fb1ca7652b5b44ecba0e26593c5078c
Ben Shimmin <bas@bas.me.uk>
2026-08-05 16:29:35 +0100

    Hook up find a person page to requirements
    Here we connect the find a person journey to the requirements page,
    with the large caveat that we have not yet built the end of the find a
    person journey, the "view appointments" page, and the routing is very
    much TBD at this point.
    
    Regardless, this commit now handles person search through to viewing
    multiple requirements, or zero requirements, but not the case of one
    requirement, which will automatically take a user through to view their
    appointments.

--- server/routes/people.ts
+++ server/routes/people.ts
@@ -1,23 +1,49 @@
 import { Router } from 'express'
+import { path } from 'static-path'
 import paths from '../paths'
 import type { Services } from '../services'
 import actions from './actions'
-import PersonSearchController from '../controllers/personSearchController'
 import { Page } from '../services/auditService'
+import requirementMiddleware from './requirementMiddleware'
+import { Controllers } from '../controllers'
 
-export default function peopleRoutes(
-  personSearchController: PersonSearchController,
-  services: Services,
-  router: Router,
-): Router {
+export default function peopleRoutes(controllers: Controllers, services: Services, router: Router): Router {
   const { get, post } = actions(router)
+  const { personSearchController, requirementController } = controllers
 
   post(paths.people.find.pattern, services.personSearchService.post)
   get(
     paths.people.find.pattern,
-    [services.personSearchService.get, personSearchController.show(Page.SEARCH_FIND_A_PERSON_RESULTS)],
+    [
+      services.personSearchService.get,
+      (req, res, next) => {
+        const resultPath = paths.people.requirement({
+          crn: ':crn',
+        })
+        return personSearchController.show(Page.SEARCH_FIND_A_PERSON_RESULTS, resultPath)(req, res, next)
+      },
+    ],
     {
       auditEvent: Page.SEARCH_FIND_A_PERSON,
+    },
+  )
+
+  // TODO: updatePath and the path for a single requirement will need
+  //       plumbing in, to connect to the (currently unbuilt) view
+  //       appointments page
+  get(
+    paths.people.requirement.pattern,
+    [
+      requirementMiddleware(services.offenderService, path('/')),
+      (req, res, next) => {
+        return requirementController.show({
+          updatePath: '/',
+          backPath: paths.people.find({}),
+        })(req, res, next)
+      },
+    ],
+    {
+      auditEvent: Page.VIEW_FIND_A_PERSON_REQUIREMENT_PAGE,
     },
   )
 
