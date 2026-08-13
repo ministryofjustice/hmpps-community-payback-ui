3aec5dd66630c5aea4f69fda07b7c8fdf76d3886
Ben Shimmin <bas@bas.me.uk>
2026-08-13 13:35:44 +0100

    Add extra code and event number to appointment client request
    When we're requesting appointments, we want to able to fetch those
    *with* an outcome as well as those without. We also want to be able
    to send an event number (based on a requirement) too.

--- server/data/appointmentClient.ts
+++ server/data/appointmentClient.ts
@@ -22,10 +22,11 @@ export type GetAppointmentsRequest = {
   toDate?: string
   outcomeCodes?: Array<AppointmentFilterOutcomeCode>
   projectTypeGroup?: ProjectTypeDto['group']
+  eventNumber?: string
 } & PagedRequest
 
 // This can be a valid contact outcome code or NO_OUTCOME
-type AppointmentFilterOutcomeCode = 'NO_OUTCOME' | 'ATTC'
+type AppointmentFilterOutcomeCode = 'NO_OUTCOME' | 'ATTC' | 'WITH_OUTCOME'
 
 export default class AppointmentClient extends RestClient {
   constructor(authenticationClient: AuthenticationClient) {
