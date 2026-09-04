910f99906a052a4dc9d05c419d6461c0d12b995b
Liz <50752284+lookupdaily@users.noreply.github.com>
2026-09-04 14:42:29 +0100

    Use new fixture in existig tests
    This make the crux of the test easier to understand, and easier to spot when it
    failed in the setup.

--- e2e-tests/tests/travel-time/task_credit_travel_time.spec.ts
+++ e2e-tests/tests/travel-time/task_credit_travel_time.spec.ts
@@ -1,60 +1,15 @@
-import { expect } from '@playwright/test'
 import { login as deliusLogin } from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/login'
 import verifyAdjustment from '@ministryofjustice/hmpps-probation-integration-e2e-tests/steps/delius/upw/verify-adjustment'
 import test from '../../fixtures/test'
-import signIn from '../../steps/signIn'
-import searchForASession from '../../steps/searchForASession'
-import selectASession from '../../steps/selectASession'
-import viewAppointmentFromList from '../../steps/viewAppointmentFromList'
-import completeCheckAppointmentDetails from '../../steps/completeCheckAppointmentDetails'
-import completeCompliance from '../../steps/completeCompliance'
-import ConfirmPage from '../../pages/appointments/confirmPage'
-import { completeAttendedCompliedOutcome } from '../../steps/completeAttendanceOutcome'
 import searchForTravelTime from '../../steps/searchForTravelTime'
 import creditTravelTime from '../../steps/creditTravelTime'
-import completeChooseSupervisor from '../../steps/completeChooseSupervisor'
-import DateTimeFormats from '../../../server/utils/dateTimeUtils'
-import completeChooseProject from '../../steps/completeChooseProject'
+import HomePage from '../../pages/homePage'
 
 test(
   'Credit travel time from task',
   { tag: '@use-group-placement-type' },
-  async ({ page, deliusUser, team, project, personOnProbation, appointment }) => {
-    await page.goto('/sign-out')
-    await expect(page.locator('h1')).toContainText('Sign in')
-
-    const homePage = await signIn(page, deliusUser)
-    const groupSessionPage = await searchForASession(page, homePage, team, appointment.date)
-
-    await groupSessionPage.expect.toSeeResults()
-
-    const sessionPage = await selectASession(page, groupSessionPage, project.name)
-
-    await sessionPage.expect.toSeeAppointments()
-
-    const checkAppointmentDetailsPage = await viewAppointmentFromList(page, sessionPage, personOnProbation.crn)
-    const chooseSupervisorPage = await completeCheckAppointmentDetails(page, checkAppointmentDetailsPage)
-    const chooseProjectPage = await completeChooseSupervisor(page, chooseSupervisorPage, team)
-    const attendanceOutcomePage = await completeChooseProject(page, chooseProjectPage)
-    const logHoursPage = await completeAttendedCompliedOutcome(page, attendanceOutcomePage)
-
-    // 4 hours of unpaid work will be required, only credit for 2 hours
-    const startTime = appointment.date.toTimeString().split(' ')[0] // appointment start time should be current time so use date from appointment fixture
-    const endTime = DateTimeFormats.addHours(DateTimeFormats.stripTime(startTime), 2)
-    await logHoursPage.endTimeFieldLocator.clear()
-    await logHoursPage.endTimeFieldLocator.fill(endTime)
-    await logHoursPage.continue()
-
-    await completeCompliance(page)
-
-    const confirmPage = new ConfirmPage(page)
-    await confirmPage.expect.toBeOnThePage()
-
-    await confirmPage.selectAlertPractitioner()
-    await confirmPage.confirmButtonLocator.click()
-
-    await sessionPage.expect.toBeOnThePage()
-
+  async ({ appointmentWithOutcome: { personOnProbation }, team, page }) => {
+    const homePage = new HomePage(page)
     await homePage.visit()
 
     const travelTimePage = await searchForTravelTime(page, homePage, team, personOnProbation)
