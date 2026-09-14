25d334f5e0f0da632e972ba27a2c162ea82371b7
Ben Shimmin <bas@bas.me.uk>
2026-09-14 12:34:29 +0100

    Introduce `backQuery` into course completions query
    We have a couple of impending new scenarios where we are
    potentially going to skip a page depending on certain circumstances.
    This will need to be handled both forwards *and* backwards, otherwise
    clicking "back" where the back page redirects forwards will result in
    the user being stuck on the page they've clicked "back" from.
    
    The easiest way to know we're going backwards is to add a query
    param to the back link which can be received by the controller
    action for the previous page, and interpreted appropriately.
    
    In this case, on the requirements page, we will redirect forwards to
    the projects page if there's only one requirement, so we need to be
    able to navigate back from the project page (hence "fromProject")
    through `requirements->show` to *its* previous page.

--- server/pages/courseCompletions/process/pathMap.ts
+++ server/pages/courseCompletions/process/pathMap.ts
@@ -9,14 +9,14 @@ export type CourseCompletionPage =
   | 'confirm'
   | 'unableToCreditTime'
 
-type NavigationPaths = { back?: CourseCompletionPage; next?: CourseCompletionPage }
+type NavigationPaths = { back?: CourseCompletionPage; next?: CourseCompletionPage; backQuery?: string }
 
 const pathMap: Record<CourseCompletionPage, NavigationPaths> = {
   crn: { next: 'person' },
   person: { back: 'crn', next: 'history' },
   history: { back: 'person', next: 'requirement' },
   requirement: { back: 'history', next: 'project' },
-  project: { back: 'requirement', next: 'appointments' },
+  project: { back: 'requirement', next: 'appointments', backQuery: 'fromProject' },
   appointments: { back: 'project', next: 'outcome' },
   outcome: { back: 'appointments', next: 'confirm' },
   confirm: { back: 'outcome' },
